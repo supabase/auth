794e9a3e96f6c5a86b8be002d681c49b1e4b1abd
Fa[K]eR?! Yuriy <mr.bes1996@mail.ru>
2020-11-17 10:31:13 +0300

    Implemented logout route

---
+++ docs/logout.go
@@ -0,0 +1,29 @@
+package docs
+
+// swagger:route POST /logout user logoutPostParams
+//
+// Logout a user.This will revoke all refresh tokens for the user. Remember that the JWT tokens will still be valid for stateless auth until they expires.
+//
+// Security:
+//  api_key:
+//  responses:
+//   204: logoutPostResponse
+//   401: httpErrorResponse
+//   400: httpErrorResponse
+//   500: httpErrorResponse
+
+// Empty logout params.
+// swagger:parameters logoutPostParams
+type logoutPostParams struct {
+	// in:body
+	Body struct {
+	}
+}
+
+// Logout response.
+// swagger:response logoutPostResponse
+type logoutPostResponse struct {
+	// in: body
+	Body struct {
+	}
+}
