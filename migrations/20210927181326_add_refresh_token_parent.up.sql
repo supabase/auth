bae42e539d82b7b256b71d6c0dde961b7dd702fd
Kang Ming <kang.ming1996@gmail.com>
2021-09-27 19:21:09 +0800

    fix(refresh_token): add parent column

---
+++ migrations/20210927181326_add_refresh_token_parent.up.sql
@@ -0,0 +1,6 @@
+-- adds parent column
+
+ALTER TABLE auth.refresh_tokens
+ADD COLUMN IF NOT EXISTS parent varchar(255) NULL,
+ADD CONSTRAINT refresh_tokens_token_unique UNIQUE ("token"),
+ADD CONSTRAINT refresh_tokens_parent_fkey FOREIGN KEY (parent) REFERENCES auth.refresh_tokens("token");
