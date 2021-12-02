e8a670c339e15934af2d546ace373effb3e7ee13
Kang Ming <kang.ming1996@gmail.com>
2021-12-02 18:44:08 +0800

    fix: update auth.uid function

---
+++ migrations/20211202183645_update_auth_uid.up.sql
@@ -0,0 +1,15 @@
+-- update auth.uid()
+
+create or replace function auth.uid()
+returns uuid
+language sql stable
+as $$
+  select
+  nullif(
+    coalesce(
+      current_setting('request.jwt.claim.sub', true),
+      (current_setting('request.jwt.claims', true)::jsonb ->> 'sub')
+    ),
+    ''
+  )::uuid
+$$;
