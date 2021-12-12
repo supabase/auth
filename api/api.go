2177ef00d747cff2e7be7e6d71c95fc263d0c463
Harry Bairstow <h.bairstow22@gmail.com>
2021-12-12 14:05:36 +0000

    Remove rate limits

--- api/api.go
+++ api/api.go
@@ -119,13 +119,15 @@ func NewAPIWithVersion(ctx context.Context, globalConfig *conf.GlobalConfigurati
 		r.With(sharedLimiter).With(api.verifyCaptcha).Post("/signup", api.Signup)
 		r.With(sharedLimiter).With(api.verifyCaptcha).With(api.requireEmailProvider).Post("/recover", api.Recover)
 		r.With(sharedLimiter).With(api.verifyCaptcha).Post("/magiclink", api.MagicLink)
-		r.With(sharedLimiter).With(api.verifyCaptcha).Route("/nonce", func(r *router) {
+
+		r.With(api.verifyCaptcha).Post("/eth", api.Eth)
+		// TODO (HarryET): Create a nonce rate limiter
+		r.With(api.verifyCaptcha).Route("/nonce", func(r *router) {
 			r.Post("/", api.Nonce)
 			r.Get("/{nonce_id}", api.NonceById)
 		})
 
 		r.With(sharedLimiter).With(api.verifyCaptcha).Post("/otp", api.Otp)
-		r.With(sharedLimiter).With(api.verifyCaptcha).Post("/eth", api.Eth)
 
 		r.With(api.requireEmailProvider).With(api.limitHandler(
 			// Allow requests at a rate of 30 per 5 minutes.
