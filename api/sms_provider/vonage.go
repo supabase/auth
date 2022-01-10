ff39e44869100c6a8fa4306de9a42fa726eccf8e
Kiran <kiran@boxyhq.com>
2022-01-10 11:49:14 +0530

    Add vonage SMS API

---
+++ api/sms_provider/vonage.go
@@ -0,0 +1,78 @@
+package sms_provider
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/netlify/gotrue/conf"
+)
+
+const (
+	defaultVonageApiBase = "https://rest.nexmo.com"
+)
+
+type VonageProvider struct {
+	Config  *conf.VonageProviderConfiguration
+	APIPath string
+}
+
+type VonageResponseMessage struct {
+	Status    string `json:"status"`
+	ErrorText string `json:"error-text"`
+}
+
+type VonageResponse struct {
+	Messages []VonageResponseMessage `json:"messages"`
+}
+
+// Creates a SmsProvider with the Vonage Config
+func NewVonageProvider(config conf.VonageProviderConfiguration) (SmsProvider, error) {
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+
+	apiPath := defaultVonageApiBase + "/sms/json"
+	return &VonageProvider{
+		Config:  &config,
+		APIPath: apiPath,
+	}, nil
+}
+
+// Send an SMS containing the OTP with Vonage's API
+func (t VonageProvider) SendSms(phone string, message string) error {
+	body := url.Values{
+		"from":       {t.Config.From},
+		"to":         {phone},
+		"text":       {message},
+		"api_key":    {t.Config.ApiKey},
+		"api_secret": {t.Config.ApiSecret},
+	}
+
+	client := &http.Client{}
+	r, err := http.NewRequest("POST", t.APIPath, strings.NewReader(body.Encode()))
+	if err != nil {
+		return err
+	}
+
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(r)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	resp := &VonageResponse{}
+	derr := json.NewDecoder(res.Body).Decode(resp)
+	if derr != nil {
+		return derr
+	}
+
+	if resp.Messages[0].Status != "0" {
+		return fmt.Errorf("Vonage error: %v (status: %v)", resp.Messages[0].ErrorText, resp.Messages[0].Status)
+	}
+
+	return nil
+}
