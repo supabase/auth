2198ca84c2e2935e8464b4315a9c4836df290703
Kang Ming <kang.ming1996@gmail.com>
2022-02-24 00:11:28 +0800

    fix: add nullif to auth functions

---
+++ migrations/20220224000811_update_auth_functions.up.sql
@@ -0,0 +1,34 @@
+-- update auth functions
+
+create or replace function auth.uid() 
+returns uuid 
+language sql stable
+as $$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.sub', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'sub')
+	)::uuid
+$$;
+
+create or replace function auth.role() 
+returns text 
+language sql stable
+as $$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.role', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'role')
+	)::text
+$$;
+
+create or replace function auth.email() 
+returns text 
+language sql stable
+as $$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.email', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'email')
+	)::text
+$$;
