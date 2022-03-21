4f0f400b90405bf8527027984d1eb779ac00ed8b
Div Arora <root@darora.com>
2022-03-21 06:54:57 +0100

    fix: no longer hardcode username for migration
    The username is configurable via the connstring, and as such we can't
    rely on it being supabase_auth_admin

--- migrations/20220320143831_set_idle_transaction_timeout.up.sql
+++ migrations/20220320143831_set_idle_transaction_timeout.up.sql
@@ -1,3 +1,3 @@
 -- set idle_in_transaction_session_timeout to 5min
 
-ALTER ROLE supabase_auth_admin SET idle_in_transaction_session_timeout TO 300000;
+ALTER ROLE current_user SET idle_in_transaction_session_timeout TO 300000;
