b560b87f0cbf358827adfff716ee9c6fa50c0157
Bariq <bariqhibat@gmail.com>
2022-04-30 23:38:09 +0800

    reviews: resolve comments
    Signed-off-by: Bariq <bariqhibat@gmail.com>

--- api/provider/orcid.go
+++ api/provider/orcid.go
@@ -28,6 +28,31 @@ type orcidUser struct {
 	LocationCountryCode string `json:"loccountrycode"`
 }
 
+type orcUser struct {
+	Emails struct {
+		Email []struct {
+			Email     string `json:"email"`
+			IsPrimary bool   `json:"primary"`
+			Verified  bool   `json:"verified"`
+		} `json:"email"`
+	} `json:"emails"`
+	Addresses struct {
+		Address []struct {
+			Country struct {
+				Value string `json:"value"`
+			} `json:"country"`
+		} `json:"address"`
+	} `json:"addresses"`
+	Name struct {
+		FirstName struct {
+			Value string `json:"value"`
+		} `json:"given-names"`
+		LastName struct {
+			Value string `json:"value"`
+		} `json:"family-name"`
+	} `json:"name"`
+}
+
 // NewOrcidProvider creates a Orcid account provider.
 func NewOrcidProvider(ext conf.OAuthProviderConfiguration) (OAuthProvider, error) {
 	if err := ext.Validate(); err != nil {
@@ -57,6 +82,15 @@ func (g orcidProvider) GetOAuthToken(code string) (*oauth2.Token, error) {
 }
 
 func (g orcidProvider) GetUserData(ctx context.Context, tok *oauth2.Token) (*UserProvidedData, error) {
+	var apiResponse orcUser
+	var primaryEmail string
+	var countryCodes []string
+	var emails []Email
+	isPrimaryExists := false
+
+	// From Authorization code token exchange, orcID would also get returned as JSON
+	// orcID would later be required for determining the route for user data retrieval
+	// Ref: https://info.orcid.org/documentation/integration-and-api-faq/#easy-faq-2537
 	orcID := fmt.Sprintf("%v", tok.Extra("orcid"))
 
 	if orcID == "" {
@@ -64,59 +98,40 @@ func (g orcidProvider) GetUserData(ctx context.Context, tok *oauth2.Token) (*Use
 	}
 
 	// API for reading public user information
-	// Docs: https://github.com/ORCID/orcid-model/tree/master/src/main/resources/record_3.0
-	apiURL := defaultOrcidPublicApi + "/v3.0/" + orcID + "/record"
+	// API Docs: https://github.com/ORCID/orcid-model/tree/master/src/main/resources/record_3.0
+	apiURL := defaultOrcidPublicApi + "/v3.0/" + orcID + "/record" // full JSON: https://github.com/ORCID/orcid-model/blob/master/src/main/resources/record_3.0/samples/read_samples/full-record-3.0.json
 
-	apiResponse := struct {
-		Emails struct {
-			Email []struct {
-				Value string `json:"email"`
-			} `json:"email"`
-		} `json:"emails"`
-		Addresses struct {
-			Address []struct {
-				Country struct {
-					Value string `json:"value"`
-				} `json:"country"`
-			} `json:"address"`
-		} `json:"addresses"`
-		Name struct {
-			FirstName struct {
-				Value string `json:"value"`
-			} `json:"given-names"`
-			LastName struct {
-				Value string `json:"value"`
-			} `json:"family-name"`
-		} `json:"name"`
-	}{}
 	if err := makeRequest(ctx, tok, g.Config, apiURL, &apiResponse); err != nil {
 		return nil, err
 	}
 	if l := len(apiResponse.Emails.Email); l < 1 {
 		return nil, errors.New("Unable to find email with Orcid provider. Please make sure that it's visible to public")
 	}
-	var countryCodes []string
 	for _, v := range apiResponse.Addresses.Address {
 		countryCodes = append(countryCodes, v.Country.Value)
 	}
-	var emails []Email
-	for i, v := range apiResponse.Emails.Email {
-		primary := false
-		if i == 0 {
-			primary = true
+	for _, v := range apiResponse.Emails.Email {
+		if v.IsPrimary {
+			isPrimaryExists = true
+			primaryEmail = v.Email
 		}
 		emails = append(emails, Email{
-			Email:    v.Value,
-			Verified: false,
-			Primary:  primary,
+			Email:    v.Email,
+			Verified: v.Verified,
+			Primary:  v.IsPrimary,
 		})
+	}
+	if !isPrimaryExists { // this means that primary email isn't visible to public
+		// hack: make the first index to be primary on Supabase
+		emails[0].Primary = true
+		primaryEmail = emails[0].Email
 	}
 	u := orcidUser{
 		ID:                  orcID,
 		FirstName:           apiResponse.Name.FirstName.Value,
 		LastName:            apiResponse.Name.LastName.Value,
 		LocationCountryCode: strings.Join(countryCodes, ","),
-		Email:               emails[0].Email,
+		Email:               primaryEmail,
 	}
 	return &UserProvidedData{
 		Metadata: &Claims{
@@ -125,7 +140,7 @@ func (g orcidProvider) GetUserData(ctx context.Context, tok *oauth2.Token) (*Use
 			Name:          strings.TrimSpace(u.FirstName + " " + u.LastName),
 			Picture:       "",
 			Email:         u.Email,
-			EmailVerified: false,
+			EmailVerified: true,
 
 			// To be deprecated
 			AvatarURL:  "",
