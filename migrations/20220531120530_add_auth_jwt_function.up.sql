e22cbc71878f052acff1e6f0aff77f52a0749488
Kang Ming <kang.ming1996@gmail.com>
2022-05-31 16:29:17 -0700

    fix: add auth.jwt() function (#484)

---
+++ migrations/20220531120530_add_auth_jwt_function.up.sql
@@ -0,0 +1,12 @@
+-- add auth.jwt function
+
+comment on function auth.uid() is 'Deprecated. Use auth.jwt() -> ''sub'' instead.';
+comment on function auth.role() is 'Deprecated. Use auth.jwt() -> ''role'' instead.';
+comment on function auth.email() is 'Deprecated. Use auth.jwt() -> ''email'' instead.';
+
+create or replace function auth.jwt()
+returns jsonb
+language sql stable
+as $$
+  select nullif(current_setting('request.jwt.claims', true), '')::jsonb
+$$;
