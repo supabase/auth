cc01122d1a7b3d60c7c6e503bf636cc7d897729f
joel@joellee.org <joel@joellee.org>
2022-06-07 16:54:27 +0800

    db: add constraints

--- migrations/20220607041349_add_mfa_schema.up.sql
+++ migrations/20220607041349_add_mfa_schema.up.sql
@@ -3,10 +3,11 @@
 DROP TYPE IF EXISTS factor_type;
 CREATE TYPE factor_type AS ENUM('phone', 'webauthn', 'email')
 CREATE TABLE IF NOT EXISTS auth.mfa_challenge(
-       id VARCHAR(256)
-       auth_factor_id VARCHAR(256)
-       created_at timestamptz
-       CONSTRAINT mfa_challenge_pkey PRIMARY KEY (id)
+       id VARCHAR(256) NOT NULL,
+       factor_id VARCHAR(256) NOT NULL,
+       created_at timestamptz NULL,
+       CONSTRAINT mfa_challenge_pkey PRIMARY KEY (id),
+       CONSTRAINT mfa_challenge_auth_factor_id_fkey FOREIGN KEY (auth_factor_id) REFERENCES auth.mfa_factors(id) ON DELETE CASCADE
 )
 
 comment on table auth.mfa_challenges is 'Auth: stores data of Multi Factor Authentication Requests';
@@ -15,16 +16,16 @@ comment on table auth.mfa_challenges is 'Auth: stores data of Multi Factor Authe
 -- auth.factors definition
 
 CREATE TABLE IF NOT EXISTS auth.mfa_factors(
-       id VARCHAR(256)
-       factor_simple_name VARCHAR(256)
-       factor_type factor_type
-       enabled BOOLEAN
-       created_at timestamptz
-       updated_at timestamptz
-       totp_email VARCHAR(256)
-       totp_phone VARCHAR(256)
-       webauthn_public_key_bytes VARCHAR(256)
-       webauthn_credential_id VARCHAR(256)
+       id VARCHAR(256) NOT NULL,
+       factor_simple_name VARCHAR(256) NUL,
+       factor_type factor_type NOT NULL,
+       enabled BOOLEAN NOT NULL,
+       created_at timestamptz NOT NULL,
+       updated_at timestamptz NULL,
+       totp_email VARCHAR(256) NULL,
+       totp_phone VARCHAR(256) NULL,
+       webauthn_public_key_bytes VARCHAR(256) NULL,
+       webauthn_credential_id VARCHAR(256) NULL,
        CONSTRAINT mfa_factors_pkey PRIMARY KEY(id)
 )
 
@@ -34,11 +35,13 @@ comment on table auth.factors is 'Auth: stores Multi Factor Authentication facto
 
 -- auth.challenge definition
 CREATE TABLE IF NOT EXISTS auth.mfa_backup_codes(
-       user_id VARCHAR(256)
-       created_at timestamptz
-       backup_code VARCHAR(32)
-       valid BOOLEAN
-       time_used timestamptz
+       user_id uuid NOT NULL,
+       created_at timestamptz NOT NULL,
+       backup_code VARCHAR(32) NOT NULL,
+       valid BOOLEAN NOT NULL,
+       time_used timestamptz NULL,
+       CONSTRAINT mfa_backup_codes_pkey PRIMARY KEY(user_id, backup_code)
+       CONSTRAINT mfa_backup_codes FOREIGN KEY(user_id) REFERENCES auth.users(id) ON DELETE CASCADE
 )
 
 comment on table auth.backup_codes is 'Auth: stores backup codes for Multi Factor Authentication';
