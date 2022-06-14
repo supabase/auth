5e10354f2172d2ad7d41a8256c0b309209cf6f06
joel@joellee.org <joel@joellee.org>
2022-06-14 22:53:00 +0800

    chore: switch db type to inet

--- migrations/20220614074223_add_ip_address_to_audit_log.postgres.up.sql
+++ migrations/20220614074223_add_ip_address_to_audit_log.postgres.up.sql
@@ -1,3 +1,3 @@
 -- Add IP Address to audit log
 ALTER TABLE auth.audit_log_entries
-ADD COLUMN IF NOT EXISTS ip_address VARCHAR(256) NOT NULL DEFAULT '';
+ADD COLUMN IF NOT EXISTS ip_address inet NULL;
