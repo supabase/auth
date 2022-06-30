684e4fc5b0272783422d58b26c944586bd4de065
joel@joellee.org <joel@joellee.org>
2022-06-30 16:26:54 +0800

    refactor: change bools to timestamps

--- migrations/20220607041349_add_mfa_schema.up.sql
+++ migrations/20220607041349_add_mfa_schema.up.sql
@@ -8,14 +8,14 @@ END $$;
 
 -- auth.mfa_factors definition
 CREATE TABLE IF NOT EXISTS auth.mfa_factors(
-       id VARCHAR(256) NOT NULL,
+       id VARCHAR(255) NOT NULL,
        user_id uuid NOT NULL,
-       friendly_name VARCHAR(256) NULL,
+       friendly_name VARCHAR(255) NULL,
        factor_type factor_type NOT NULL,
        status factor_status NOT NULL,
        created_at timestamptz NOT NULL,
        updated_at timestamptz NOT NULL,
-       secret_key VARCHAR(256) NOT NULL,
+       secret_key VARCHAR(255) NOT NULL,
        UNIQUE(user_id, friendly_name),
        CONSTRAINT mfa_factors_pkey PRIMARY KEY(id),
        CONSTRAINT mfa_factors_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id) ON DELETE CASCADE
@@ -24,10 +24,10 @@ comment on table auth.mfa_factors is 'Auth: stores metadata about factors';
 
 -- auth.mfa_challenges definition
 CREATE TABLE IF NOT EXISTS auth.mfa_challenges(
-       id VARCHAR(256) NOT NULL,
-       factor_id VARCHAR(256) NOT NULL,
+       id VARCHAR(255) NOT NULL,
+       factor_id VARCHAR(255) NOT NULL,
        created_at timestamptz NOT NULL,
-       verified BOOLEAN NOT NULL,
+       verified_at timestamptz NOT NULL,
        CONSTRAINT mfa_challenges_pkey PRIMARY KEY (id),
        CONSTRAINT mfa_challenges_auth_factor_id_fkey FOREIGN KEY (factor_id) REFERENCES auth.mfa_factors(id) ON DELETE CASCADE
 );
@@ -35,10 +35,9 @@ comment on table auth.mfa_challenges is 'Auth: stores metadata about challenge r
 
 -- auth.mfa_recovery_codes definition
 CREATE TABLE IF NOT EXISTS auth.mfa_recovery_codes(
-       id serial PRIMARY KEY,
+       id bigint generated always as identity,
        user_id uuid NOT NULL,
        recovery_code VARCHAR(32) NOT NULL,
-       valid BOOLEAN NOT NULL,
        created_at timestamptz NOT NULL,
        used_at timestamptz NOT NULL,
        CONSTRAINT mfa_recovery_codes_user_id_recovery_code_pkey UNIQUE(user_id, recovery_code),
