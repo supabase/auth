1fd73fabf68cda1c91ffae08fd5110bcc37bee1b
Artem Vysotsky <artem.vysotsky@gmail.com>
2022-07-18 23:57:56 -0400

    fix migration

--- migrations/20210710035447_alter_users.up.sql
+++ migrations/20210710035447_alter_users.up.sql
@@ -7,13 +7,5 @@ ADD COLUMN IF NOT EXISTS phone_change VARCHAR(15) NULL DEFAULT '',
 ADD COLUMN IF NOT EXISTS phone_change_token VARCHAR(255) NULL DEFAULT '',
 ADD COLUMN IF NOT EXISTS phone_change_sent_at timestamptz NULL DEFAULT NULL;
 
-DO $$
-BEGIN
-  IF NOT EXISTS(SELECT *
-    FROM information_schema.columns
-    WHERE table_schema = 'auth' and table_name='users' and column_name='email_confirmed_at')
-  THEN
-      ALTER TABLE "users" RENAME COLUMN "confirmed_at" TO "email_confirmed_at";
-  END IF;
-END $$;
+ALTER TABLE "users" RENAME COLUMN "confirmed_at" TO "email_confirmed_at";
 
--- migrations/20210730183235_add_email_change_confirmed.up.sql
+++ migrations/20210730183235_add_email_change_confirmed.up.sql
@@ -4,12 +4,4 @@ ALTER TABLE users
 ADD COLUMN IF NOT EXISTS email_change_token_current varchar(255) null DEFAULT '', 
 ADD COLUMN IF NOT EXISTS email_change_confirm_status smallint DEFAULT 0 CHECK (email_change_confirm_status >= 0 AND email_change_confirm_status <= 2);
 
-DO $$
-BEGIN
-  IF NOT EXISTS(SELECT *
-    FROM information_schema.columns
-    WHERE table_schema = 'auth' and table_name='users' and column_name='email_change_token_new')
-  THEN
-      ALTER TABLE "users" RENAME COLUMN "email_change_token" TO "email_change_token_new";
-  END IF;
-END $$;
+ALTER TABLE "users" RENAME COLUMN "email_change_token" TO "email_change_token_new";
