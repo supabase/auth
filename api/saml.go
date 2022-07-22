c07b13adf375e81035f126fe391c4b5f2288e3e2
Stojan Dimitrovski <--local>
2022-07-22 11:14:26 +0200

    feat(saml): Add SSO models, basic ACS

--- api/saml.go
+++ api/saml.go
@@ -1,12 +1,16 @@
 package api
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/xml"
 	"net/http"
 	"net/url"
 
 	"github.com/crewjam/saml"
 	"github.com/crewjam/saml/samlsp"
+	"github.com/netlify/gotrue/api/provider"
+	"github.com/netlify/gotrue/models"
 )
 
 // getSAMLServiceProvider generates a new service provider object with the
@@ -44,4 +48,75 @@ func (a *API) SAMLMetadata(w http.ResponseWriter, r *http.Request) error {
 	_, err = w.Write(metadataXML)
 
 	return err
+}
+
+// samlCallback implements the main Assertion Consumer Service endpoint behavior.
+func (a *API) samlCallback(ctx context.Context, r *http.Request) (*provider.UserProvidedData, error) {
+	if r.FormValue("SAMLart") != "" {
+		// TODO: SAML Artifact callbacks are only possible when you can
+		// identify the IdP via RelayState i.e. only the SP initiated
+		// flow is supported
+		return nil, badRequestError("SAML Response with artifacts not supported at this time")
+	}
+
+	samlResponse := r.FormValue("SAMLResponse")
+	if samlResponse == "" {
+		return nil, badRequestError("SAMLResponse is missing")
+	}
+
+	responseXML, err := base64.StdEncoding.DecodeString(samlResponse)
+	if err != nil {
+		return nil, badRequestError("SAMLResponse is not a valid Base64 string")
+	}
+
+	var peekAssertion saml.Assertion
+	err = xml.Unmarshal(responseXML, peekAssertion)
+	if err != nil {
+		return nil, badRequestError("SAMLResponse is not a valid XML SAML assertion")
+	}
+
+	entityId := peekAssertion.Issuer.Value
+
+	ssoProvider, err := models.FindSAMLProviderForEntityID(a.db, entityId)
+	if err != nil {
+		return nil, err
+	}
+
+	if ssoProvider == nil {
+		return nil, badRequestError("SAML Assertion has unrecognized Issuer")
+	}
+
+	idpMetadata, err := samlsp.ParseMetadata([]byte(ssoProvider.SAMLProvider.MetadataXML))
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: fetch new metadata if possible when validUntil < time.Now()
+
+	serviceProvider := a.getSAMLServiceProvider(idpMetadata)
+	assertion, err := serviceProvider.ParseResponse(r, nil)
+	if err != nil {
+		return nil, badRequestError("SAML Assertion is not valid")
+	}
+
+	var email string
+
+	for _, stmt := range assertion.AttributeStatements {
+		for _, attr := range stmt.Attributes {
+			if attr.Name == "email" {
+				email = attr.Values[0].Value
+				break
+			}
+		}
+	}
+
+	return &provider.UserProvidedData{
+		Emails: []provider.Email{
+			{
+				Email:    email,
+				Verified: true,
+				Primary:  true,
+			},
+		},
+	}, nil
 }
