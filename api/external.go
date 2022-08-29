30ffbbb736864c48fa690c1d0c275bdf8aedb39b
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2022-08-29 10:11:52 +0200

    docs: add reference link to the OAuth2 spec

--- api/external.go
+++ api/external.go
@@ -291,6 +291,7 @@ func (a *API) internalExternalProviderCallback(w http.ResponseWriter, r *http.Re
 		q := url.Values{}
 		q.Set("provider_token", providerAccessToken)
 		// Because not all providers give out a refresh token
+		// See corresponding OAuth2 spec: <https://www.rfc-editor.org/rfc/rfc6749.html#section-5.1>
 		if providerRefreshToken != "" {
 			q.Set("provider_refresh_token", providerRefreshToken)
 		}
