d0a5966217e7f539f48fc6ef37f165553447ddd3
Joel Lee <lee.yi.jie.joel@gmail.com>
2022-08-29 21:52:52 +0530

    feat: cherry-pick step up changes onto separate branch (#652)
    * refactor: cherry pick onto separate branch
    
    * fix: update recovery code files
    
    * feat: cherry-pick partial changes from stepup login branch
    
    * Update models/recovery_code.go
    
    Co-authored-by: joel@joellee.org <joel@joellee.org>

--- models/recovery_code_test.go
+++ models/recovery_code_test.go
@@ -61,3 +61,17 @@ func (ts *RecoveryCodeTestSuite) createRecoveryCode(u *User) *RecoveryCode {
 	require.NoError(ts.T(), err)
 	return rc
 }
+
+// Create Recovery Code
+func (ts *RecoveryCodeTestSuite) TestConsumedRecoveryCodesAreNotValid() {
+	user, err := NewUser("", "", "", "", nil)
+	err = ts.db.Create(user)
+	rc := ts.createRecoveryCode(user)
+	isRCValid, err := IsRecoveryCodeValid(ts.db, user, rc.RecoveryCode)
+	require.NoError(ts.T(), err)
+	require.Equal(ts.T(), true, isRCValid)
+	err = rc.Consume(ts.db)
+	require.NoError(ts.T(), err)
+	isRCValid, err = IsRecoveryCodeValid(ts.db, user, rc.RecoveryCode)
+	require.Equal(ts.T(), false, isRCValid)
+}
