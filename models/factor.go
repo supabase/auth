ddbf27c4b8260944ae4158ae82d3aa416812f56a
joel@joellee.org <joel@joellee.org>
2022-10-03 14:51:39 +0300

    Revert "fix: remove belongs to association on factor"
    This reverts commit 10057f71a371b09eefc1bec7037cba114c7244b6.

--- models/factor.go
+++ models/factor.go
@@ -16,11 +16,12 @@ const Webauthn = "webauthn"
 
 type Factor struct {
 	ID           uuid.UUID `json:"id" db:"id"`
+	User         User      `belongs_to:"user"`
 	UserID       uuid.UUID `json:"user_id" db:"user_id"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
 	Status       string    `json:"status" db:"status"`
-	FriendlyName string    `json:"friendly_name,omitempty" db:"friendly_name"`
+	FriendlyName string    `json:"friendly_name" db:"friendly_name"`
 	TOTPSecret   string    `json:"-" db:"totp_secret"`
 	FactorType   string    `json:"factor_type" db:"factor_type"`
 }
