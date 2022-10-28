0182feb8d91a27fbf163488382a8191664a69ce5
Kang Ming <kang.ming1996@gmail.com>
2022-10-28 16:54:10 -0700

    fix: return user not found error

--- api/auth.go
+++ api/auth.go
@@ -97,7 +97,10 @@ func (a *API) maybeLoadUserOrSession(ctx context.Context) (context.Context, erro
 			return ctx, badRequestError("invalid claim: sub claim must be a UUID").WithInternalError(err)
 		}
 		user, err = models.FindUserByID(db, userId)
-		if err != nil && !models.IsNotFoundError(err) {
+		if err != nil {
+			if models.IsNotFoundError(err) {
+				return ctx, notFoundError(err.Error())
+			}
 			return ctx, err
 		}
 		ctx = withUser(ctx, user)
