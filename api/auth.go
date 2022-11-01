2ea68bdc37f063953b7d8f8ab8c096c8667a6161
Kang Ming <kang.ming1996@gmail.com>
2022-11-01 00:24:25 -0700

    refactor: remove redundant param

--- api/auth.go
+++ api/auth.go
@@ -33,7 +33,7 @@ func (a *API) requireAuthentication(w http.ResponseWriter, r *http.Request) (con
 	return ctx, err
 }
 
-func (a *API) requireAdmin(ctx context.Context, w http.ResponseWriter, r *http.Request) (context.Context, error) {
+func (a *API) requireAdmin(ctx context.Context, r *http.Request) (context.Context, error) {
 	// Find the administrative user
 	claims := getClaims(ctx)
 	if claims == nil {
