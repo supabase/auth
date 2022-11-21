87719f7621c81ffc1459554024d9bd11c06d7611
joel@joellee.org <joel@joellee.org>
2022-11-21 10:26:26 +0200

    fix: update migration

--- migrations/20221011041400_add_mfa_indexes.up.sql
+++ migrations/20221011041400_add_mfa_indexes.up.sql
@@ -2,7 +2,7 @@ create index if not exists refresh_token_session_id on {{ index .Options "Namesp
 
 alter table {{ index .Options "Namespace" }}.mfa_amr_claims
   add column if not exists id uuid not null,
-  add constraint amr_id_pk primary key(id);
+  add constraint if not exists amr_id_pk primary key(id);
 
 create index if not exists user_id_created_at_idx on {{ index .Options "Namespace" }}.sessions (user_id, created_at);
 create index if not exists factor_id_created_at_idx on {{ index .Options "Namespace" }}.mfa_factors (user_id, created_at);
