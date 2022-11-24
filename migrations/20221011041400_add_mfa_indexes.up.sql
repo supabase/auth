9a6a2711ab65e1709608bacf938ceb86d454f179
joel@joellee.org <joel@joellee.org>
2022-11-24 19:16:58 +0200

    fix: remove plpgsql

--- migrations/20221011041400_add_mfa_indexes.up.sql
+++ migrations/20221011041400_add_mfa_indexes.up.sql
@@ -1,17 +1,16 @@
 alter table {{ index .Options "Namespace" }}.mfa_amr_claims
   add column if not exists id uuid not null;
 
-do language plpgsql
-$$ declare
+do $$
 begin
-    if not exists ( select constraint_name
-                from    information_schema.check_constraints
-                where   constraint_schema = '{{ index .Options "Namespace" }}'
-                  and   constraint_name = 'amr_id_pk'
-              )
-    then
-        execute 'alter table {{ index .Options "Namespace" }}.mfa_amr_claims add constraint amr_id_pk primary key(id)';
-    end if;
+  if not exists
+     (select constraint_name
+      from information_schema.check_constraints
+      where constraint_schema = '{{ index .Options "Namespace" }}'
+      and constraint_name = 'amr_id_pk')
+  then
+    alter table {{ index .Options "Namespace" }}.mfa_amr_claims add constraint amr_id_pk primary key(id);
+  end if;
 end $$;
 
 create index if not exists user_id_created_at_idx on {{ index .Options "Namespace" }}.sessions (user_id, created_at);
