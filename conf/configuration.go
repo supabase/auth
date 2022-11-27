51d040776a8ba4768e412da18ddbfbd941a4a896
Eunsoo Shin <me@esinx.net>
2022-11-27 23:23:03 +0900

    feat(kakao):add Kakao Provider

--- conf/configuration.go
+++ conf/configuration.go
@@ -142,6 +142,7 @@ type ProviderConfiguration struct {
 	Github      OAuthProviderConfiguration `json:"github"`
 	Gitlab      OAuthProviderConfiguration `json:"gitlab"`
 	Google      OAuthProviderConfiguration `json:"google"`
+	Kakao       OAuthProviderConfiguration `json:"kakao"`
 	Notion      OAuthProviderConfiguration `json:"notion"`
 	Keycloak    OAuthProviderConfiguration `json:"keycloak"`
 	Linkedin    OAuthProviderConfiguration `json:"linkedin"`
