17a01d2b81bd100ea6b9d7743a6a52a4ff987286
Kang Ming <kang.ming1996@gmail.com>
2022-12-23 15:48:44 -0800

    add multi-tenant migration

---
+++ cmd/migrate_multi_tenant_cmd.go
@@ -0,0 +1,106 @@
+package cmd
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/pop/v5/logging"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var migrateMultiTenantCmd = cobra.Command{
+	Use:  "migrate-multi-tenant",
+	Long: "Migrate command for the multi-tenant database.",
+	Run:  migrateMultiTenant,
+}
+
+func migrateMultiTenant(cmd *cobra.Command, args []string) {
+	tenantConfig := loadTenantConfig(cmd.Context())
+	u, err := url.Parse(tenantConfig.MultiTenantConfiguration.URL)
+	if err != nil {
+		logrus.Fatalf("%+v", errors.Wrap(err, "parsing db connection url"))
+	}
+
+	log := logrus.StandardLogger()
+
+	pop.Debug = false
+	if tenantConfig.Logging.Level != "" {
+		level, err := logrus.ParseLevel(tenantConfig.Logging.Level)
+		if err != nil {
+			log.Fatalf("Failed to parse log level: %+v", err)
+		}
+		log.SetLevel(level)
+		if level == logrus.DebugLevel {
+			// Set to true to display query info
+			pop.Debug = true
+		}
+		if level != logrus.DebugLevel {
+			var noopLogger = func(lvl logging.Level, s string, args ...interface{}) {
+			}
+			// Hide pop migration logging
+			pop.SetLogger(noopLogger)
+		}
+	}
+
+	processedUrl := tenantConfig.MultiTenantConfiguration.URL
+	if len(u.Query()) != 0 {
+		processedUrl = fmt.Sprintf("%s&application_name=gotrue_multi_tenant_migrations", processedUrl)
+	} else {
+		processedUrl = fmt.Sprintf("%s?application_name=gotrue_multi_tenant_migrations", processedUrl)
+	}
+	deets := &pop.ConnectionDetails{
+		URL: processedUrl,
+	}
+	deets.Options = map[string]string{
+		"migration_table_name": "schema_migrations",
+		"Namespace":            "public",
+	}
+
+	db, err := pop.NewConnection(deets)
+	if err != nil {
+		log.Fatalf("%+v", errors.Wrap(err, "opening db connection"))
+	}
+	defer db.Close()
+
+	if err := db.Open(); err != nil {
+		log.Fatalf("%+v", errors.Wrap(err, "checking database connection"))
+	}
+
+	migrationPath := "./migrations/multitenant"
+	log.Debugf("Reading migrations from %s", migrationPath)
+	mig, err := pop.NewFileMigrator(migrationPath, db)
+	if err != nil {
+		log.Fatalf("%+v", errors.Wrap(err, "creating db migrator"))
+	}
+	log.Debugf("before status")
+
+	if log.Level == logrus.DebugLevel {
+		err = mig.Status(os.Stdout)
+		if err != nil {
+			log.Fatalf("%+v", errors.Wrap(err, "migration status"))
+		}
+	}
+
+	// turn off schema dump
+	mig.SchemaPath = ""
+
+	err = mig.Up()
+	if err != nil {
+		log.Fatalf("%v", errors.Wrap(err, "running db migrations"))
+	} else {
+		log.Infof("GoTrue multi-tenant migrations applied successfully")
+	}
+
+	log.Debugf("after status")
+
+	if log.Level == logrus.DebugLevel {
+		err = mig.Status(os.Stdout)
+		if err != nil {
+			log.Fatalf("%+v", errors.Wrap(err, "migration status"))
+		}
+	}
+}
