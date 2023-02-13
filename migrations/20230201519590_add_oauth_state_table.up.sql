47daaae9cd5fb0831e5c36d97738e8ea02ea0c4c
joel@joellee.org <joel@joellee.org>
2023-02-13 21:17:02 +0800

    fix: update varchar->text in migrations

--- migrations/20230201519590_add_oauth_state_table.up.sql
+++ migrations/20230201519590_add_oauth_state_table.up.sql
@@ -1,12 +1,11 @@
 -- auth.oauth_state definition
 create table if not exists {{ index .Options "Namespace" }}.oauth_state(
        id uuid primary key,
-       internal_auth_code varchar(255) null,
-       hashed_code_challenge varchar(255) unique null,
-       provider_type varchar(255) null,
+       internal_auth_code text null,
+       hashed_code_challenge text null,
+       provider_type text null,
        redirect_uri text null,
        created_at timestamptz null,
        updated_at timestamptz null,
-       unique(provider_type, hashed_code_challenge)
 );
 comment on table {{ index .Options "Namespace" }}.oauth_state is 'stores metadata for oauth provider logins';
