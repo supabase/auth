e8b42e03acf815b995d88044998541e85fb81bcc
Stojan Dimitrovski <sdimitrovski@gmail.com>
2023-03-14 12:17:02 +0800

    feat: make phone data type alter backward compatible

--- migrations/20230116124310_alter_phone_type.up.sql
+++ migrations/20230116124310_alter_phone_type.up.sql
@@ -1,5 +1,12 @@
 -- alter phone field column type to accomodate for soft deletion 
 
-alter table auth.users
-alter column phone type text,
-alter column phone_change type text;
+do $$
+begin
+  alter table {{ index .Options "Namespace" }}.users
+    alter column phone type text,
+    alter column phone_change type text;
+exception
+  -- dependent object: https://www.postgresql.org/docs/current/errcodes-appendix.html
+  when SQLSTATE '2BP01' then
+    raise notice 'Unable to change data type of phone, phone_change columns due to dependent objects';
+end $$;
