f203f0b7d8b0f2d6c99e4c1e5d20988a3a36125b
meetchaudhari <meet.devms@gmail.com>
2023-03-20 19:13:44 +0530

    changes

--- internal/api/sms_provider/msg91.go
+++ internal/api/sms_provider/msg91.go
@@ -1,50 +1,51 @@
 package sms_provider
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"strconv"
-	"strings"
 
 	"github.com/supabase/gotrue/internal/conf"
+	"github.com/supabase/gotrue/internal/utilities"
 )
 
-var err_codes = map[string]int{
-	"Missing mobile number": 101,
-	"Missing message":       102,
-	"Missing username":      104,
-	"Missing password":      105,
-	"Invalid username or password. 201 also appears in case the XML code triggered is incorrect.": 201,
-	"Invalid mobile number":                                         202,
-	"Invalid sender ID or DLT Entity Id Missing":                    203,
-	"Invalid authentication key":                                    207,
-	"IP is blacklisted":                                             208,
-	"Default route not found":                                       209,
-	"The route could not be determined. Please contact support":     210,
-	"DLT Template Id Missing":                                       211,
-	"The user does not have sufficient balance to send SMS":         301,
-	"Expired user account":                                          302,
-	"Banned user account":                                           303,
-	"This route is currently unavailable":                           306,
-	"The schedule time is incorrect":                                307,
-	"Campaign name cannot be more than 32 characters":               308,
-	"The selected group(s) does not belong to you":                  309,
-	"SMS is too long. The system paused this request automatically": 310,
-	"When the same SMS content is sent to the same number within 10 seconds. This is a security feature used to avoid multiple deliveries. The first SMS will be delivered and the second will be rejected. The balance will also be deducted only once.": 311,
-	"Flow ID Missing or Invalid Flow":                                 400,
-	"Flow Not Yet Approved":                                           401,
-	"Flow is disabled":                                                403,
-	"IP not whitelisted":                                              418,
-	"Internal error, please contact your account manager":             506,
-	"If your current route is disabled, kindly select another route":  602,
-	"This sender ID is blacklisted, please use a different sender ID": 603,
-	"Please enter at least one correct number to send an SMS":         604,
-	"The scheduled date cannot be more than three weeks":              606,
-	"Please enter the campaign name":                                  607,
-	"Scheduled SMS cannot be less than the current end time":          608,
-}
+// var err_codes = map[string]int{
+// 	"Missing mobile number": 101,
+// 	"Missing message":       102,
+// 	"Missing username":      104,
+// 	"Missing password":      105,
+// 	"Invalid username or password. 201 also appears in case the XML code triggered is incorrect.": 201,
+// 	"Invalid mobile number":                                         202,
+// 	"Invalid sender ID or DLT Entity Id Missing":                    203,
+// 	"Invalid authentication key":                                    207,
+// 	"IP is blacklisted":                                             208,
+// 	"Default route not found":                                       209,
+// 	"The route could not be determined. Please contact support":     210,
+// 	"DLT Template Id Missing":                                       211,
+// 	"The user does not have sufficient balance to send SMS":         301,
+// 	"Expired user account":                                          302,
+// 	"Banned user account":                                           303,
+// 	"This route is currently unavailable":                           306,
+// 	"The schedule time is incorrect":                                307,
+// 	"Campaign name cannot be more than 32 characters":               308,
+// 	"The selected group(s) does not belong to you":                  309,
+// 	"SMS is too long. The system paused this request automatically": 310,
+// 	"When the same SMS content is sent to the same number within 10 seconds. This is a security feature used to avoid multiple deliveries. The first SMS will be delivered and the second will be rejected. The balance will also be deducted only once.": 311,
+// 	"Flow ID Missing or Invalid Flow":                                 400,
+// 	"Flow Not Yet Approved":                                           401,
+// 	"Flow is disabled":                                                403,
+// 	"IP not whitelisted":                                              418,
+// 	"Internal error, please contact your account manager":             506,
+// 	"If your current route is disabled, kindly select another route":  602,
+// 	"This sender ID is blacklisted, please use a different sender ID": 603,
+// 	"Please enter at least one correct number to send an SMS":         604,
+// 	"The scheduled date cannot be more than three weeks":              606,
+// 	"Please enter the campaign name":                                  607,
+// 	"Scheduled SMS cannot be less than the current end time":          608,
+// }
 
 const (
 	defaultMsg91ApiBase = "https://api.msg91.com/api/sendhttp.php?pluginsource=68"
@@ -56,7 +57,8 @@ type Msg91Provider struct {
 }
 
 type Msg91Response struct {
-	Data string `json:"data"`
+	Message string `json:"message"`
+	Type    string `json:"type"`
 }
 
 // NewMsg91Provider creates a new SmsProvider for Msg91.
@@ -83,17 +85,27 @@ func (t *Msg91Provider) SendMessage(phone string, message string, channel string
 func (t *Msg91Provider) SendSms(phone string, message string) error {
 
 	body := url.Values{
-		"authkey":   {t.Config.AuthKey},
-		"sender":    {t.Config.SenderId},
-		"DLT_TE_ID": {t.Config.DltTemplateId},
-		"mobiles":   {phone},
-		"message":   {message},
-		"route":     {strconv.Itoa(4)},
+		"authkey":  {t.Config.AuthKey},
+		"sender":   {t.Config.SenderId},
+		"mobiles":  {phone},
+		"message":  {message},
+		"route":    {strconv.Itoa(4)},
+		"response": {"json"},
+	}
+
+	// DLT template ID is only required for Indian Users , to comply with government regulations
+	// Indian users have to get their sms template approved by DLT authorities before using it.
+	// DLT template ID is provided by Authorities after the template is approved.
+	if t.Config.DltTemplateId != nil && *t.Config.DltTemplateId != "" {
+		body.Set("DLT_TE_ID", *t.Config.DltTemplateId)
 	}
 
 	client := &http.Client{Timeout: defaultTimeout}
 
-	r, err := http.NewRequest("POST", t.APIPath, strings.NewReader(body.Encode()))
+	bodyBuffer := bytes.NewBufferString(body.Encode())
+
+	r, err := http.NewRequest("POST", t.APIPath, bodyBuffer)
+
 	if err != nil {
 		return err
 	}
@@ -106,16 +118,17 @@ func (t *Msg91Provider) SendSms(phone string, message string) error {
 		return err
 	}
 
-	resBody, _ := io.ReadAll(res.Body)
+	defer utilities.SafeClose(res.Body)
 
-	resBodyString := string(resBody)
-	resBodyString = strings.Replace(resBodyString, ".", "", -1)
-
-	if value, ok := err_codes[resBodyString]; ok {
-		return fmt.Errorf("Msg91 error: %v (code: %v)", resBodyString, value)
-
-	} else {
-		return nil
+	resp := &Msg91Response{}
+	derr := json.NewDecoder(res.Body).Decode(resp)
+	if derr != nil {
+		return derr
 	}
 
+	if resp.Type == "success" {
+		return nil
+	} else {
+		return fmt.Errorf("Msg91 error: %v (code: %v)", resp.Message, res.StatusCode)
+	}
 }
