0f47f445a0927a6160075582c464cbc778f6389e
Kang Ming <kang.ming1996@gmail.com>
2023-03-21 11:22:05 +0800

    fix: always include updated_at

--- internal/storage/sql.go
+++ internal/storage/sql.go
@@ -1,5 +1,6 @@
 package storage
 
 func (conn *Connection) UpdateOnly(model interface{}, includeColumns ...string) error {
+	includeColumns = append(includeColumns, "updated_at")
 	return conn.UpdateColumns(model, includeColumns...)
 }
