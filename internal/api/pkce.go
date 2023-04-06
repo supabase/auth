c9e2212b675834b8a9769c705b618ae1243cf6f3
Joel Lee <lee.yi.jie.joel@gmail.com>
2023-04-06 18:13:14 +0800

    Apply suggestions from code review
    Co-authored-by: Stojan Dimitrovski <sdimitrovski@gmail.com>

--- internal/api/pkce.go
+++ internal/api/pkce.go
@@ -9,15 +9,15 @@ const PKCE = "pkce"
 const MinCodeChallengeLength = 43
 const MaxCodeChallengeLength = 128
 
-var codeChallengePattern = regexp.MustCompile("^[a-zA-Z-._~0-9]+$")
+var codeChallengePattern = regexp.MustCompile("^[a-zA-Z._~0-9-]+$")
 
 func isValidCodeChallenge(codeChallenge string) (bool, error) {
 	// See RFC 7636 Section 4.2: https://www.rfc-editor.org/rfc/rfc7636#section-4.2
-	hasValidChallengeChars := codeChallengePattern.MatchString
+	
 	switch codeChallengeLength := len(codeChallenge); {
 	case codeChallengeLength < MinCodeChallengeLength, codeChallengeLength > MaxCodeChallengeLength:
 		return false, badRequestError("code challenge has to be between %v and %v characters", MinCodeChallengeLength, MaxCodeChallengeLength)
-	case !hasValidChallengeChars(codeChallenge):
+	case !codeChallengePattern.MatchString(codeChallenge):
 		return false, badRequestError("code challenge can only contain alphanumeric characters, hyphens, periods, underscores and tildes")
 	default:
 		return true, nil
