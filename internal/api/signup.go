a9c0a5b93dea2b298bfafd952431031400af7e2a
Joel Lee <lee.yi.jie.joel@gmail.com>
2023-04-07 20:14:36 +0800

    Update internal/api/signup.go
    Co-authored-by: Stojan Dimitrovski <sdimitrovski@gmail.com>

--- internal/api/signup.go
+++ internal/api/signup.go
@@ -184,7 +184,7 @@ func (a *API) Signup(w http.ResponseWriter, r *http.Request) error {
 				}); terr != nil {
 					return terr
 				}
-				flowState, terr := models.NewFlowStateWithUserID(params.Provider, params.CodeChallenge, codeChallengeMethod, models.OTP, &(user.ID))
+				flowState, terr := models.NewFlowStateWithUserID(params.Provider, params.CodeChallenge, codeChallengeMethod, models.OTP, &user.ID)
 				if terr != nil {
 					return terr
 				}
