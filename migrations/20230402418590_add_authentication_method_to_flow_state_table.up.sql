6974bbe2cd52af1f98bce6d5acb7a8b180b13766
joel@joellee.org <joel@joellee.org>
2023-04-17 10:50:16 +0800

    fix: make migration idempotent, add index

--- migrations/20230402418590_add_authentication_method_to_flow_state_table.up.sql
+++ migrations/20230402418590_add_authentication_method_to_flow_state_table.up.sql
@@ -1,2 +1,3 @@
 alter table {{index .Options "Namespace" }}.flow_state
-add column authentication_method text not null;
+add column if not exists authentication_method text not null;
+create index if not exists idx_user_id_auth_method on {{index .Options "Namespace" }}.flow_state (user_id, authentication_method);
