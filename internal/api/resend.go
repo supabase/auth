184fa38f0f90b7a7c6d7c9c4c8a1a087f5e9b453
Kang Ming <kang.ming1996@gmail.com>
2023-05-08 18:08:33 +0200

    fix: resend email change & phone change issues (#1100)
    ## What kind of change does this PR introduce?
    * Fix #1095 where `/resend` doesn't work if the user initially signed up
    with a phone number and is trying to resend an email change email

--- internal/api/resend.go
+++ internal/api/resend.go
@@ -27,10 +27,10 @@ func (p *ResendConfirmationParams) Validate() error {
 		return badRequestError("Missing one of these types: signup, email_change, sms, phone_change")
 
 	}
-	if p.Email == "" && (p.Type == signupVerification || p.Type == emailChangeVerification) {
+	if p.Email == "" && p.Type == signupVerification {
 		return badRequestError("Type provided requires an email address")
 	}
-	if p.Phone == "" && (p.Type == smsVerification || p.Type == phoneChangeVerification) {
+	if p.Phone == "" && p.Type == smsVerification {
 		return badRequestError("Type provided requires a phone number")
 	}
 
@@ -132,13 +132,13 @@ func (a *API) Resend(w http.ResponseWriter, r *http.Request) error {
 			}
 			return a.sendPhoneConfirmation(ctx, tx, user, params.Phone, phoneConfirmationOtp, smsProvider, sms_provider.SMSProvider)
 		case emailChangeVerification:
-			return a.sendEmailChange(tx, config, user, mailer, params.Email, referrer, config.Mailer.OtpLength, models.ImplicitFlow)
+			return a.sendEmailChange(tx, config, user, mailer, user.EmailChange, referrer, config.Mailer.OtpLength, models.ImplicitFlow)
 		case phoneChangeVerification:
 			smsProvider, terr := sms_provider.GetSmsProvider(*config)
 			if terr != nil {
 				return terr
 			}
-			return a.sendPhoneConfirmation(ctx, tx, user, params.Phone, phoneChangeVerification, smsProvider, sms_provider.SMSProvider)
+			return a.sendPhoneConfirmation(ctx, tx, user, user.PhoneChange, phoneChangeVerification, smsProvider, sms_provider.SMSProvider)
 		}
 		return nil
 	})
