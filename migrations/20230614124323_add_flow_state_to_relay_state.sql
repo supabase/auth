108f932e3939c7914e431dc1302536a0a5a54b60
joel@joellee.org <joel@joellee.org>
2023-06-16 17:10:23 +0800

    fix: update openapi spec

--- migrations/20230614124323_add_flow_state_to_relay_state.sql
+++ migrations/20230614124323_add_flow_state_to_relay_state.sql
@@ -1,4 +1,1 @@
-
-
-alter table saml_relay_states
-  add column if not exists flow_state_id uuid references flow_states(id);
+alter table {{ index .Options "Namespace" }}.saml_relay_states add column if not exists flow_state_id uuid;
