4b7f1ff840e7f5accccb768c8125c1a0ffc35603
joel@joellee.org <joel@joellee.org>
2023-06-20 21:26:36 +0800

    fix: modify twilio verify to return VerificationSID

--- internal/api/sms_provider/twilio_verify.go
+++ internal/api/sms_provider/twilio_verify.go
@@ -21,12 +21,13 @@ type TwilioVerifyProvider struct {
 }
 
 type VerificationResponse struct {
-	To           string `json:"to"`
-	Status       string `json:"status"`
-	Channel      string `json:"channel"`
-	Valid        bool   `json:"valid"`
-	ErrorCode    string `json:"error_code"`
-	ErrorMessage string `json:"error_message"`
+	To              string `json:"to"`
+	Status          string `json:"status"`
+	Channel         string `json:"channel"`
+	Valid           bool   `json:"valid"`
+	VerificationSID string `json:"sid"`
+	ErrorCode       string `json:"error_code"`
+	ErrorMessage    string `json:"error_message"`
 }
 
 // See: https://www.twilio.com/docs/verify/api/verification-check
@@ -55,14 +56,14 @@ func NewTwilioVerifyProvider(config conf.TwilioVerifyProviderConfiguration) (Sms
 func (t *TwilioVerifyProvider) SendMessage(phone string, message string, channel string) (string, error) {
 	switch channel {
 	case SMSProvider, WhatsappProvider:
-		return "", t.SendSms(phone, message, channel)
+		return t.SendSms(phone, message, channel)
 	default:
 		return "", fmt.Errorf("channel type %q is not supported for Twilio", channel)
 	}
 }
 
 // Send an SMS containing the OTP with Twilio's API
-func (t *TwilioVerifyProvider) SendSms(phone, message, channel string) error {
+func (t *TwilioVerifyProvider) SendSms(phone, message, channel string) (string, error) {
 	// Unlike Programmable Messaging, Verify does not require a prefix for channel
 	receiver := "+" + phone
 	body := url.Values{
@@ -72,23 +73,29 @@ func (t *TwilioVerifyProvider) SendSms(phone, message, channel string) error {
 	client := &http.Client{Timeout: defaultTimeout}
 	r, err := http.NewRequest("POST", t.APIPath, strings.NewReader(body.Encode()))
 	if err != nil {
-		return err
+		return "", err
 	}
 	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	r.SetBasicAuth(t.Config.AccountSid, t.Config.AuthToken)
 	res, err := client.Do(r)
 	defer utilities.SafeClose(res.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 	if !(res.StatusCode == http.StatusOK || res.StatusCode == http.StatusCreated) {
 		resp := &twilioErrResponse{}
 		if err := json.NewDecoder(res.Body).Decode(resp); err != nil {
-			return err
+			return "", err
 		}
-		return resp
+		return "", resp
 	}
-	return nil
+
+	resp := &VerificationResponse{}
+	derr := json.NewDecoder(res.Body).Decode(resp)
+	if derr != nil {
+		return "", derr
+	}
+	return resp.VerificationSID, nil
 }
 
 func (t *TwilioVerifyProvider) VerifyOTP(phone, code string) error {
