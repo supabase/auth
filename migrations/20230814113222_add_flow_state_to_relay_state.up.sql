cecb39c9d027e16ad3fed78ee606836de524e6dd
joel@joellee.org <joel@joellee.org>
2023-08-18 18:00:15 +0200

    fix: remove accidentally commiteed old migration

--- migrations/20230814113222_add_flow_state_to_relay_state.up.sql
+++
@@ -1,1 +0,0 @@
-alter table {{ index .Options "Namespace" }}.saml_relay_states add column if not exists flow_state_id uuid references {{ index .Options "Namespace" }}.flow_state(id) on delete cascade default null;
