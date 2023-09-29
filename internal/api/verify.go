cca9f33ad34e7007465d8830c6b3620136b40b6f
joel@joellee.org <joel@joellee.org>
2023-09-29 18:07:53 +0800

    fix: remove stray lines

--- internal/api/verify.go
+++ internal/api/verify.go
@@ -610,7 +610,6 @@ func (a *API) verifyUserAndToken(ctx context.Context, conn *storage.Connection, 
 			isOtpValid(tokenHash, user.EmailChangeTokenNew, user.EmailChangeSentAt, config.Mailer.OtpExp)
 	case phoneChangeVerification:
 		if config.Sms.IsTwilioVerifyProvider() {
-
 			if err := smsProvider.(*sms_provider.TwilioVerifyProvider).VerifyOTP(user.PhoneChange, params.Token); err != nil {
 				return nil, expiredTokenError("Token has expired or is invalid").WithInternalError(err)
 			}
@@ -619,7 +618,6 @@ func (a *API) verifyUserAndToken(ctx context.Context, conn *storage.Connection, 
 		isValid = isOtpValid(tokenHash, user.PhoneChangeToken, user.PhoneChangeSentAt, config.Sms.OtpExp)
 	case smsVerification:
 		if config.Sms.IsTwilioVerifyProvider() {
-
 			if err := smsProvider.(*sms_provider.TwilioVerifyProvider).VerifyOTP(params.Phone, params.Token); err != nil {
 				return nil, expiredTokenError("Token has expired or is invalid").WithInternalError(err)
 			}
