32b1a8d4ba598b22f8206346272038443dc17b5c
joel@joellee.org <joel@joellee.org>
2023-11-27 18:17:58 +0800

    fix: update test structure

--- internal/hooks/auth_hooks_test.go
+++ internal/hooks/auth_hooks_test.go
@@ -1,6 +1,7 @@
 package hooks
 
 import (
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"github.com/supabase/gotrue/internal/conf"
 	"testing"
@@ -15,12 +16,12 @@ func TestHooks(t *testing.T) {
 	suite.Run(t, ts)
 }
 
-func TestFetchHookName(ts *testing.T) {
+func (ts *HookTestSuite) TestFetchHookName() {
 	cases := []struct {
 		desc           string
 		uri            string
 		expectedResult string
-		expectedError  string // Using string to represent the error for simplicity
+		expectedError  string
 	}{
 		// Positive test cases
 		{desc: "Valid URI", uri: "pg-functions://postgres/auth/verification_hook_reject", expectedResult: "auth.verification_hook_reject", expectedError: ""},
@@ -30,18 +31,19 @@ func TestFetchHookName(ts *testing.T) {
 		{desc: "Invalid Schema Name", uri: "pg-functions://postgres/123auth/verification_hook_reject", expectedResult: "", expectedError: "invalid schema name: 123auth"},
 		{desc: "Invalid Function Name", uri: "pg-functions://postgres/auth/123verification_hook_reject", expectedResult: "", expectedError: "invalid table name: 123verification_hook_reject"},
 		{desc: "Insufficient Path Parts", uri: "pg-functions://postgres/auth", expectedResult: "", expectedError: "URI path does not contain enough parts"},
-		// {desc: "Incorrect URI Format", uri: "http://postgres/auth/verification_hook_reject", expectedResult: "", expectedError: "invalid URI format"},
 	}
 
 	for _, tc := range cases {
-		ts.Run(tc.desc, func(t *testing.T) {
+		ts.T().Run(tc.desc, func(t *testing.T) {
 			ep := conf.ExtensibilityPointConfiguration{URI: tc.uri}
 			result, err := FetchHookName(ep)
-			if err != nil && err.Error() != tc.expectedError {
-				t.Errorf("Test %s failed: expected error %v, got %v", tc.desc, tc.expectedError, err)
-			}
-			if result != tc.expectedResult {
-				t.Errorf("Test %s failed: expected result %v, got %v", tc.desc, tc.expectedResult, result)
+			if tc.expectedError == "" {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedResult, result)
+			} else {
+				require.Error(t, err)
+				require.EqualError(t, err, tc.expectedError)
+				require.Empty(t, result)
 			}
 		})
 	}
