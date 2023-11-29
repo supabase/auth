951dbadd3686a6089a30432c45fb891931e86ecb
Kang Ming <kang.ming1996@gmail.com>
2023-11-29 15:24:48 -0800

    feat: pg function should use fully qualified name

--- migrations/20231117164230_add_id_pkey_identities.up.sql
+++ migrations/20231117164230_add_id_pkey_identities.up.sql
@@ -11,7 +11,7 @@ end$$;
 
 alter table if exists {{ index .Options "Namespace" }}.identities 
     drop constraint if exists identities_pkey,
-    add column if not exists id uuid default gen_random_uuid() primary key;
+    add column if not exists id uuid default public.gen_random_uuid() primary key;
 
 do $$
 begin
