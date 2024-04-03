supabase/auth

commit e4beea1cdb80544b0581f1882696a698fdf64938

feat: add error codes (#1377)
Adds proper error codes with API versioning.

From now on, all responses that end in a 4XX HTTP status code will
include a textual description of the error that occurred.

Error responses on API versions before `2024-01-01` have this schema:

```json
{
  "code": "<http status code>",
  "msg": "<descriptive error message>",
  "error_code": "<textual error code>"
}
```

Error responses on API version on or after `2024-01-01` have this
schema:

```json
{
  "code": "<textual error code>",
  "message": "<descriptive error message>"
}
```

API versions are controlled by submitting an `X-Supabase-Api-Version`
header to the request. A missing or invalid value assumes the "initial"
API version as used before the introduction of API version `2024-01-01`.

Error code contract for API version `2024-01-01`:

1. Error codes will not be renamed. You can safely rely on them.
2. HTTP status codes are _mostly_ fixed, but you should not rely on them
except the class 4XX vs 5XX.
3. Error messages are a _developer aid._ They may change across deployed
version. You should not rely on them, but if you want you can show them
to your users. Error translations should be based on the error code!

Of the 4XX HTTP status code class, only these codes are allowed to be
used in API version `2024-01-01` according to these rules. The purpose
of this is to keep proper HTTP semantics. The tuple `(http_status_code,
error_code)` shouldn't be used by clients!

<table>
<thead class="simple-table-header"><tr
id="865bd663-796b-4bdc-a53d-8aecbdc35ab5"><th id="=Aef"
class="simple-table-header-color simple-table-header"
style="width:142.9666748046875px">HTTP Status Code</th><th id="UuUa"
class="simple-table-header-color simple-table-header"
style="width:390px">When to use?</th><th>Primary Fault
At</th></tr></thead><tbody><tr
id="794cf424-14a8-449c-880f-b6073206e70b"><td id="=Aef" class=""

internal/api/mail.go
@@ -66,14 +66,17 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 	user, err := models.FindUserByEmailAndAudience(db, params.Email, aud)
 	if err != nil {
 		if models.IsNotFoundError(err) {
-			if params.Type == magicLinkVerification {
+			switch params.Type {
+			case magicLinkVerification:
 				params.Type = signupVerification
 				params.Password, err = password.Generate(64, 10, 1, false, true)
 				if err != nil {
-					return internalServerError("error creating user").WithInternalError(err)
+					// password generation must always succeed
+					panic(err)
 				}
-			} else if params.Type == recoveryVerification || params.Type == "email_change_current" || params.Type == "email_change_new" {
-				return notFoundError(err.Error())
+
+			default:
+				return notFoundError(ErrorCodeUserNotFound, "User with this email not found")
 			}
 		} else {
 			return internalServerError("Database error finding user").WithInternalError(err)
@@ -84,7 +87,8 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 	now := time.Now()
 	otp, err := crypto.GenerateOtp(config.Mailer.OtpLength)
 	if err != nil {
-		return err
+		// OTP generation must always succeed
+		panic(err)
 	}
 
 	hashedToken := crypto.GenerateTokenHash(params.Email, otp)
@@ -118,11 +122,14 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 			}
 			user.RecoveryToken = hashedToken
 			user.RecoverySentAt = &now
-			terr = errors.Wrap(tx.UpdateOnly(user, "recovery_token", "recovery_sent_at"), "Database error updating user for recovery")
+			terr = tx.UpdateOnly(user, "recovery_token", "recovery_sent_at")
+			if terr != nil {
+				terr = errors.Wrap(terr, "Database error updating user for recovery")
+			}
 		case inviteVerification:
 			if user != nil {
 				if user.IsConfirmed() {
-					return unprocessableEntityError(DuplicateEmailMsg)
+					return unprocessableEntityError(ErrorCodeEmailExists, DuplicateEmailMsg)
 				}
 			} else {
 				signupParams := &SignupParams{
@@ -162,11 +169,14 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 			user.ConfirmationToken = hashedToken
 			user.ConfirmationSentAt = &now
 			user.InvitedAt = &now
-			terr = errors.Wrap(tx.UpdateOnly(user, "confirmation_token", "confirmation_sent_at", "invited_at"), "Database error updating user for invite")
+			terr = tx.UpdateOnly(user, "confirmation_token", "confirmation_sent_at", "invited_at")
+			if terr != nil {
+				terr = errors.Wrap(terr, "Database error updating user for invite")
+			}
 		case signupVerification:
 			if user != nil {
 				if user.IsConfirmed() {
-					return unprocessableEntityError(DuplicateEmailMsg)
+					return unprocessableEntityError(ErrorCodeEmailExists, DuplicateEmailMsg)
 				}
 				if err := user.UpdateUserMetaData(tx, params.Data); err != nil {
 					return internalServerError("Database error updating user").WithInternalError(err)
@@ -191,19 +201,22 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 			}
 			user.ConfirmationToken = hashedToken
 			user.ConfirmationSentAt = &now
-			terr = errors.Wrap(tx.UpdateOnly(user, "confirmation_token", "confirmation_sent_at"), "Database error updating user for confirmation")
+			terr = tx.UpdateOnly(user, "confirmation_token", "confirmation_sent_at")
+			if terr != nil {
+				terr = errors.Wrap(terr, "Database error updating user for confirmation")
+			}
 		case "email_change_current", "email_change_new":
 			if !config.Mailer.SecureEmailChangeEnabled && params.Type == "email_change_current" {
-				return unprocessableEntityError("Enable secure email change to generate link for current email")
+				return badRequestError(ErrorCodeValidationFailed, "Enable secure email change to generate link for current email")
 			}
 			params.NewEmail, terr = validateEmail(params.NewEmail)
 			if terr != nil {
-				return unprocessableEntityError("The new email address provided is invalid")
+				return terr
 			}
 			if duplicateUser, terr := models.IsDuplicatedEmail(tx, params.NewEmail, user.Aud, user); terr != nil {
 				return internalServerError("Database error checking email").WithInternalError(terr)
 			} else if duplicateUser != nil {
-				return unprocessableEntityError(DuplicateEmailMsg)
+				return unprocessableEntityError(ErrorCodeEmailExists, DuplicateEmailMsg)
 			}
 			now := time.Now()
 			user.EmailChangeSentAt = &now
@@ -214,9 +227,12 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 			} else if params.Type == "email_change_new" {
 				user.EmailChangeTokenNew = crypto.GenerateTokenHash(params.NewEmail, otp)
 			}
-			terr = errors.Wrap(tx.UpdateOnly(user, "email_change_token_current", "email_change_token_new", "email_change", "email_change_sent_at", "email_change_confirm_status"), "Database error updating user for email change")
+			terr = tx.UpdateOnly(user, "email_change_token_current", "email_change_token_new", "email_change", "email_change_sent_at", "email_change_confirm_status")
+			if terr != nil {
+				terr = errors.Wrap(terr, "Database error updating user for email change")
+			}
 		default:
-			return badRequestError("Invalid email action link type requested: %v", params.Type)
+			return badRequestError(ErrorCodeValidationFailed, "Invalid email action link type requested: %v", params.Type)
 		}
 
 		if terr != nil {
@@ -255,7 +271,8 @@ func sendConfirmation(tx *storage.Connection, u *models.User, mailer mailer.Mail
 	oldToken := u.ConfirmationToken
 	otp, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeeed
+		panic(err)
 	}
 	token := crypto.GenerateTokenHash(u.GetEmail(), otp)
 	u.ConfirmationToken = addFlowPrefixToToken(token, flowType)
@@ -265,7 +282,12 @@ func sendConfirmation(tx *storage.Connection, u *models.User, mailer mailer.Mail
 		return errors.Wrap(err, "Error sending confirmation email")
 	}
 	u.ConfirmationSentAt = &now
-	return errors.Wrap(tx.UpdateOnly(u, "confirmation_token", "confirmation_sent_at"), "Database error updating user for confirmation")
+	err = tx.UpdateOnly(u, "confirmation_token", "confirmation_sent_at")
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for confirmation")
+	}
+
+	return nil
 }
 
 func sendInvite(tx *storage.Connection, u *models.User, mailer mailer.Mailer, referrerURL string, externalURL *url.URL, otpLength int) error {
@@ -273,7 +295,8 @@ func sendInvite(tx *storage.Connection, u *models.User, mailer mailer.Mailer, re
 	oldToken := u.ConfirmationToken
 	otp, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeed
+		panic(err)
 	}
 	u.ConfirmationToken = crypto.GenerateTokenHash(u.GetEmail(), otp)
 	now := time.Now()
@@ -283,7 +306,12 @@ func sendInvite(tx *storage.Connection, u *models.User, mailer mailer.Mailer, re
 	}
 	u.InvitedAt = &now
 	u.ConfirmationSentAt = &now
-	return errors.Wrap(tx.UpdateOnly(u, "confirmation_token", "confirmation_sent_at", "invited_at"), "Database error updating user for invite")
+	err = tx.UpdateOnly(u, "confirmation_token", "confirmation_sent_at", "invited_at")
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for invite")
+	}
+
+	return nil
 }
 
 func (a *API) sendPasswordRecovery(tx *storage.Connection, u *models.User, mailer mailer.Mailer, maxFrequency time.Duration, referrerURL string, externalURL *url.URL, otpLength int, flowType models.FlowType) error {
@@ -295,7 +323,8 @@ func (a *API) sendPasswordRecovery(tx *storage.Connection, u *models.User, maile
 	oldToken := u.RecoveryToken
 	otp, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeed
+		panic(err)
 	}
 	token := crypto.GenerateTokenHash(u.GetEmail(), otp)
 	u.RecoveryToken = addFlowPrefixToToken(token, flowType)
@@ -305,7 +334,12 @@ func (a *API) sendPasswordRecovery(tx *storage.Connection, u *models.User, maile
 		return errors.Wrap(err, "Error sending recovery email")
 	}
 	u.RecoverySentAt = &now
-	return errors.Wrap(tx.UpdateOnly(u, "recovery_token", "recovery_sent_at"), "Database error updating user for recovery")
+	err = tx.UpdateOnly(u, "recovery_token", "recovery_sent_at")
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for recovery")
+	}
+
+	return nil
 }
 
 func (a *API) sendReauthenticationOtp(tx *storage.Connection, u *models.User, mailer mailer.Mailer, maxFrequency time.Duration, otpLength int) error {
@@ -317,7 +351,8 @@ func (a *API) sendReauthenticationOtp(tx *storage.Connection, u *models.User, ma
 	oldToken := u.ReauthenticationToken
 	otp, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeed
+		panic(err)
 	}
 	u.ReauthenticationToken = crypto.GenerateTokenHash(u.GetEmail(), otp)
 	now := time.Now()
@@ -326,7 +361,12 @@ func (a *API) sendReauthenticationOtp(tx *storage.Connection, u *models.User, ma
 		return errors.Wrap(err, "Error sending reauthentication email")
 	}
 	u.ReauthenticationSentAt = &now
-	return errors.Wrap(tx.UpdateOnly(u, "reauthentication_token", "reauthentication_sent_at"), "Database error updating user for reauthentication")
+	err = tx.UpdateOnly(u, "reauthentication_token", "reauthentication_sent_at")
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for reauthentication")
+	}
+
+	return nil
 }
 
 func (a *API) sendMagicLink(tx *storage.Connection, u *models.User, mailer mailer.Mailer, maxFrequency time.Duration, referrerURL string, externalURL *url.URL, otpLength int, flowType models.FlowType) error {
@@ -339,7 +379,8 @@ func (a *API) sendMagicLink(tx *storage.Connection, u *models.User, mailer maile
 	oldToken := u.RecoveryToken
 	otp, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeed
+		panic(err)
 	}
 	token := crypto.GenerateTokenHash(u.GetEmail(), otp)
 	u.RecoveryToken = addFlowPrefixToToken(token, flowType)
@@ -350,7 +391,12 @@ func (a *API) sendMagicLink(tx *storage.Connection, u *models.User, mailer maile
 		return errors.Wrap(err, "Error sending magic link email")
 	}
 	u.RecoverySentAt = &now
-	return errors.Wrap(tx.UpdateOnly(u, "recovery_token", "recovery_sent_at"), "Database error updating user for recovery")
+	err = tx.UpdateOnly(u, "recovery_token", "recovery_sent_at")
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for recovery")
+	}
+
+	return nil
 }
 
 // sendEmailChange sends out an email change token to the new email.
@@ -361,7 +407,8 @@ func (a *API) sendEmailChange(tx *storage.Connection, config *conf.GlobalConfigu
 	}
 	otpNew, err := crypto.GenerateOtp(otpLength)
 	if err != nil {
-		return err
+		// OTP generation must succeed
+		panic(err)
 	}
 	u.EmailChange = email
 	token := crypto.GenerateTokenHash(u.EmailChange, otpNew)
@@ -371,7 +418,8 @@ func (a *API) sendEmailChange(tx *storage.Connection, config *conf.GlobalConfigu
 	if config.Mailer.SecureEmailChangeEnabled && u.GetEmail() != "" {
 		otpCurrent, err = crypto.GenerateOtp(otpLength)
 		if err != nil {
-			return err
+			// OTP generation must succeed
+			panic(err)
 		}
 		currentToken := crypto.GenerateTokenHash(u.GetEmail(), otpCurrent)
 		u.EmailChangeTokenCurrent = addFlowPrefixToToken(currentToken, flowType)
@@ -384,22 +432,28 @@ func (a *API) sendEmailChange(tx *storage.Connection, config *conf.GlobalConfigu
 	}
 
 	u.EmailChangeSentAt = &now
-	return errors.Wrap(tx.UpdateOnly(
+	err = tx.UpdateOnly(
 		u,
 		"email_change_token_current",
 		"email_change_token_new",
 		"email_change",
 		"email_change_sent_at",
 		"email_change_confirm_status",
-	), "Database error updating user for email change")
+	)
+
+	if err != nil {
+		return errors.Wrap(err, "Database error updating user for email change")
+	}
+
+	return nil
 }
 
 func validateEmail(email string) (string, error) {
 	if email == "" {
-		return "", unprocessableEntityError("An email address is required")
+		return "", badRequestError(ErrorCodeValidationFailed, "An email address is required")
 	}
 	if err := checkmail.ValidateFormat(email); err != nil {
-		return "", unprocessableEntityError("Unable to validate email address: " + err.Error())
+		return "", badRequestError(ErrorCodeValidationFailed, "Unable to validate email address: "+err.Error())
 	}
 	return strings.ToLower(email), nil
 }

.github/workflows/conventional-commits-lint.js
@@ -46,7 +46,12 @@ let failed = false;
 
 validate.forEach((payload) => {
   if (payload.title) {
-    const { groups } = payload.title.match(TITLE_PATTERN);
+    const match = payload.title.match(TITLE_PATTERN);
+    if (!match) {
+      return
+    }
+
+    const { groups } = match
 
     if (groups) {
       if (groups.breaking) {

internal/api/admin.go
@@ -50,7 +50,7 @@ func (a *API) loadUser(w http.ResponseWriter, r *http.Request) (context.Context,
 
 	userID, err := uuid.FromString(chi.URLParam(r, "user_id"))
 	if err != nil {
-		return nil, badRequestError("user_id must be an UUID")
+		return nil, notFoundError(ErrorCodeValidationFailed, "user_id must be an UUID")
 	}
 
 	observability.LogEntrySetField(r, "user_id", userID)
@@ -58,7 +58,7 @@ func (a *API) loadUser(w http.ResponseWriter, r *http.Request) (context.Context,
 	u, err := models.FindUserByID(db, userID)
 	if err != nil {
 		if models.IsNotFoundError(err) {
-			return nil, notFoundError("User not found")
+			return nil, notFoundError(ErrorCodeUserNotFound, "User not found")
 		}
 		return nil, internalServerError("Database error loading user").WithInternalError(err)
 	}
@@ -69,7 +69,7 @@ func (a *API) loadUser(w http.ResponseWriter, r *http.Request) (context.Context,
 func (a *API) loadFactor(w http.ResponseWriter, r *http.Request) (context.Context, error) {
 	factorID, err := uuid.FromString(chi.URLParam(r, "factor_id"))
 	if err != nil {
-		return nil, badRequestError("factor_id must be an UUID")
+		return nil, notFoundError(ErrorCodeValidationFailed, "factor_id must be an UUID")
 	}
 
 	observability.LogEntrySetField(r, "factor_id", factorID)
@@ -77,7 +77,7 @@ func (a *API) loadFactor(w http.ResponseWriter, r *http.Request) (context.Contex
 	f, err := models.FindFactorByFactorID(a.db, factorID)
 	if err != nil {
 		if models.IsNotFoundError(err) {
-			return nil, notFoundError("Factor not found")
+			return nil, notFoundError(ErrorCodeMFAFactorNotFound, "Factor not found")
 		}
 		return nil, internalServerError("Database error loading factor").WithInternalError(err)
 	}
@@ -101,12 +101,12 @@ func (a *API) adminUsers(w http.ResponseWriter, r *http.Request) error {
 
 	pageParams, err := paginate(r)
 	if err != nil {
-		return badRequestError("Bad Pagination Parameters: %v", err)
+		return badRequestError(ErrorCodeValidationFailed, "Bad Pagination Parameters: %v", err).WithInternalError(err)
 	}
 
 	sortParams, err := sort(r, map[string]bool{models.CreatedAt: true}, []models.SortField{{Name: models.CreatedAt, Dir: models.Descending}})
 	if err != nil {
-		return badRequestError("Bad Sort Parameters: %v", err)
+		return badRequestError(ErrorCodeValidationFailed, "Bad Sort Parameters: %v", err)
 	}
 
 	filter := r.URL.Query().Get("filter")
@@ -160,7 +160,7 @@ func (a *API) adminUserUpdate(w http.ResponseWriter, r *http.Request) error {
 		if params.BanDuration != "none" {
 			duration, err = time.ParseDuration(params.BanDuration)
 			if err != nil {
-				return badRequestError("invalid format for ban duration: %v", err)
+				return badRequestError(ErrorCodeValidationFailed, "invalid format for ban duration: %v", err)
 			}
 		}
 		if terr := user.Ban(a.db, duration); terr != nil {
@@ -308,7 +308,7 @@ func (a *API) adminUserCreate(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	if params.Email == "" && params.Phone == "" {
-		return unprocessableEntityError("Cannot create a user without either an email or phone")
+		return badRequestError(ErrorCodeValidationFailed, "Cannot create a user without either an email or phone")
 	}
 
 	var providers []string
@@ -320,7 +320,7 @@ func (a *API) adminUserCreate(w http.ResponseWriter, r *http.Request) error {
 		if user, err := models.IsDuplicatedEmail(db, params.Email, aud, nil); err != nil {
 			return internalServerError("Database error checking email").WithInternalError(err)
 		} else if user != nil {
-			return unprocessableEntityError(DuplicateEmailMsg)
+			return unprocessableEntityError(ErrorCodeEmailExists, DuplicateEmailMsg)
 		}
 		providers = append(providers, "email")
 	}
@@ -333,7 +333,7 @@ func (a *API) adminUserCreate(w http.ResponseWriter, r *http.Request) error {
 		if exists, err := models.IsDuplicatedPhone(db, params.Phone, aud); err != nil {
 			return internalServerError("Database error checking phone").WithInternalError(err)
 		} else if exists {
-			return unprocessableEntityError("Phone number already registered by another user")
+			return unprocessableEntityError(ErrorCodePhoneExists, "Phone number already registered by another user")
 		}
 		providers = append(providers, "phone")
 	}
@@ -429,7 +429,7 @@ func (a *API) adminUserCreate(w http.ResponseWriter, r *http.Request) error {
 			if params.BanDuration != "none" {
 				duration, err = time.ParseDuration(params.BanDuration)
 				if err != nil {
-					return badRequestError("invalid format for ban duration: %v", err)
+					return badRequestError(ErrorCodeValidationFailed, "invalid format for ban duration: %v", err)
 				}
 			}
 			if terr := user.Ban(a.db, duration); terr != nil {
@@ -460,11 +460,11 @@ func (a *API) adminUserDelete(w http.ResponseWriter, r *http.Request) error {
 	params := &adminUserDeleteParams{}
 	body, err := getBodyBytes(r)
 	if err != nil {
-		return badRequestError("Could not read body").WithInternalError(err)
+		return internalServerError("Could not read body").WithInternalError(err)
 	}
 	if len(body) > 0 {
 		if err := json.Unmarshal(body, params); err != nil {
-			return badRequestError("Could not read params: %v", err)
+			return badRequestError(ErrorCodeBadJSON, "Could not read params: %v", err)
 		}
 	} else {
 		params.ShouldSoftDelete = false
@@ -559,6 +559,7 @@ func (a *API) adminUserUpdateFactor(w http.ResponseWriter, r *http.Request) erro
 	user := getUser(ctx)
 	adminUser := getAdminUser(ctx)
 	params := &adminUserUpdateFactorParams{}
+
 	if err := retrieveRequestParams(r, params); err != nil {
 		return err
 	}
@@ -571,7 +572,7 @@ func (a *API) adminUserUpdateFactor(w http.ResponseWriter, r *http.Request) erro
 		}
 		if params.FactorType != "" {
 			if params.FactorType != models.TOTP {
-				return badRequestError("Factor Type not valid")
+				return badRequestError(ErrorCodeValidationFailed, "Factor Type not valid")
 			}
 			if terr := factor.UpdateFactorType(tx, params.FactorType); terr != nil {
 				return terr

internal/api/anonymous.go
@@ -15,7 +15,7 @@ func (a *API) SignupAnonymously(w http.ResponseWriter, r *http.Request) error {
 	aud := a.requestAud(ctx, r)
 
 	if config.DisableSignup {
-		return forbiddenError("Signups not allowed for this instance")
+		return unprocessableEntityError(ErrorCodeSignupDisabled, "Signups not allowed for this instance")
 	}
 
 	params := &SignupParams{}

internal/api/api.go
@@ -155,7 +155,7 @@ func NewAPIWithVersion(ctx context.Context, globalConfig *conf.GlobalConfigurati
 				}
 				if params.Email == "" && params.Phone == "" {
 					if !api.config.External.AnonymousUsers.Enabled {
-						return unprocessableEntityError("Anonymous sign-ins are disabled")
+						return unprocessableEntityError(ErrorCodeAnonymousProviderDisabled, "Anonymous sign-ins are disabled")
 					}
 					if _, err := api.limitHandler(limiter)(w, r); err != nil {
 						return err

internal/api/apiversions.go
@@ -1,0 +1,35 @@
+package api
+
+import (
+	"time"
+)
+
+const APIVersionHeaderName = "X-Supabase-Api-Version"
+
+type APIVersion = time.Time
+
+var (
+	APIVersionInitial  = time.Time{}
+	APIVersion20240101 = time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC)
+)
+
+func DetermineClosestAPIVersion(date string) (APIVersion, error) {
+	if date == "" {
+		return APIVersionInitial, nil
+	}
+
+	parsed, err := time.ParseInLocation("2006-01-02", date, time.UTC)
+	if err != nil {
+		return APIVersionInitial, err
+	}
+
+	if parsed.Compare(APIVersion20240101) >= 0 {
+		return APIVersion20240101, nil
+	}
+
+	return APIVersionInitial, nil
+}
+
+func FormatAPIVersion(apiVersion APIVersion) string {
+	return apiVersion.Format("2006-01-02")
+}

internal/api/apiversions_test.go
@@ -1,0 +1,29 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDetermineClosestAPIVersion(t *testing.T) {
+	version, err := DetermineClosestAPIVersion("")
+	require.NoError(t, err)
+	require.Equal(t, APIVersionInitial, version)
+
+	version, err = DetermineClosestAPIVersion("Not a date")
+	require.Error(t, err)
+	require.Equal(t, APIVersionInitial, version)
+
+	version, err = DetermineClosestAPIVersion("2023-12-31")
+	require.NoError(t, err)
+	require.Equal(t, APIVersionInitial, version)
+
+	version, err = DetermineClosestAPIVersion("2024-01-01")
+	require.NoError(t, err)
+	require.Equal(t, APIVersion20240101, version)
+
+	version, err = DetermineClosestAPIVersion("2024-01-02")
+	require.NoError(t, err)
+	require.Equal(t, APIVersion20240101, version)
+}

internal/api/audit.go
@@ -20,7 +20,7 @@ func (a *API) adminAuditLog(w http.ResponseWriter, r *http.Request) error {
 	// aud := a.requestAud(ctx, r)
 	pageParams, err := paginate(r)
 	if err != nil {
-		return badRequestError("Bad Pagination Parameters: %v", err)
+		return badRequestError(ErrorCodeValidationFailed, "Bad Pagination Parameters: %v", err)
 	}
 
 	var col []string
@@ -31,7 +31,7 @@ func (a *API) adminAuditLog(w http.ResponseWriter, r *http.Request) error {
 		qparts := strings.SplitN(q, ":", 2)
 		col, exists = filterColumnMap[qparts[0]]
 		if !exists || len(qparts) < 2 {
-			return badRequestError("Invalid query scope: %s", q)
+			return badRequestError(ErrorCodeValidationFailed, "Invalid query scope: %s", q)
 		}
 		qval = qparts[1]
 	}

commit 4fc388186ac7e7a9a32ca9b963a83d6ac2eb7603

fix: generate signup link should not error (#1514)
## What kind of change does this PR introduce?
* Fixes an error introduced in #1377 where generating a signup link for
a new user results in a "User with this email not found" error

## What is the current behavior?

Please link any relevant issues here.

## What is the new behavior?

Feel free to include screenshots if it includes visual changes.

## Additional context

Add any other context or screenshots.

internal/api/mail.go
@@ -1,10 +1,11 @@
 package api
 
 import (
-	mail "github.com/supabase/auth/internal/mailer"
 	"net/http"
 	"strings"
 	"time"
+
+	mail "github.com/supabase/auth/internal/mailer"
 
 	"github.com/badoux/checkmail"
 	"github.com/fatih/structs"
@@ -72,8 +73,7 @@ func (a *API) adminGenerateLink(w http.ResponseWriter, r *http.Request) error {
 					// password generation must always succeed
 					panic(err)
 				}
-
-			default:
+			case mail.RecoveryVerification, mail.EmailChangeCurrentVerification, mail.EmailChangeNewVerification:
 				return notFoundError(ErrorCodeUserNotFound, "User with this email not found")
 			}
 		} else {

internal/api/mail_test.go
@@ -65,7 +65,19 @@ func (ts *MailTestSuite) TestGenerateLink() {
 		ExpectedResponse map[string]interface{}
 	}{
 		{
-			Desc: "Generate signup link",
+			Desc: "Generate signup link for new user",
+			Body: GenerateLinkParams{
+				Email:    "new_user@example.com",
+				Password: "secret123",
+				Type:     "signup",
+			},
+			ExpectedCode: http.StatusOK,
+			ExpectedResponse: map[string]interface{}{
+				"redirect_to": ts.Config.SiteURL,
+			},
+		},
+		{
+			Desc: "Generate signup link for existing user",
 			Body: GenerateLinkParams{
 				Email:    "test@example.com",
 				Password: "secret123",