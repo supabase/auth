afce41882cb639d60697537f17477ac6a4a26b29
Kang Ming <kang.ming1996@gmail.com>
2024-05-15 15:21:58 +0800

    chore: add test for recoverer

--- internal/api/errors_test.go
+++ internal/api/errors_test.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
+	"github.com/supabase/auth/internal/conf"
+	"github.com/supabase/auth/internal/observability"
 )
 
 func TestHandleResponseErrorWithHTTPError(t *testing.T) {
@@ -61,4 +66,40 @@ func TestHandleResponseErrorWithHTTPError(t *testing.T) {
 		require.Equal(t, example.HTTPError.HTTPStatus, rec.Code)
 		require.Equal(t, example.ExpectedBody, rec.Body.String())
 	}
+}
+
+func TestRecoverer(t *testing.T) {
+	var logBuffer bytes.Buffer
+	config, err := conf.LoadGlobal(apiTestConfig)
+	require.NoError(t, err)
+	require.NoError(t, observability.ConfigureLogging(&config.Logging))
+
+	// logrus should write to the buffer so we can check if the logs are output correctly
+	logrus.SetOutput(&logBuffer)
+	panicHandler := recoverer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("test panic")
+	}))
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "http://example.com", nil)
+	require.NoError(t, err)
+
+	panicHandler.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusInternalServerError, w.Code)
+
+	var data HTTPError
+
+	// panic should return an internal server error
+	require.NoError(t, json.NewDecoder(w.Body).Decode(&data))
+	require.Equal(t, ErrorCodeUnexpectedFailure, data.ErrorCode)
+	require.Equal(t, http.StatusInternalServerError, data.HTTPStatus)
+	require.Equal(t, "Internal Server Error", data.Message)
+
+	// panic should log the error message internally
+	var logs map[string]interface{}
+	require.NoError(t, json.NewDecoder(&logBuffer).Decode(&logs))
+	require.Equal(t, "request panicked", logs["msg"])
+	require.Equal(t, "test panic", logs["panic"])
+	require.NotEmpty(t, logs["stack"])
 }
