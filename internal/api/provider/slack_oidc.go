6e73216f9e58a86e56cd8db124f98b31baad7954
Zach Hawtof <zhawtof@gmail.com>
2024-06-04 22:23:45 -0400

    Leverage unique Slack OIDC helper functions

--- internal/api/provider/slack_oidc.go
+++ internal/api/provider/slack_oidc.go
@@ -1,20 +1,37 @@
 package provider
 
 import (
+	"context"
 	"strings"
 
 	"github.com/supabase/auth/internal/conf"
 	"golang.org/x/oauth2"
 )
 
-// NewSlackProvider creates a Slack account provider.
+const defaultSlackOIDCApiBase = "slack.com"
+
+type slackOIDCProvider struct {
+	*oauth2.Config
+	APIPath string
+}
+
+type slackOIDCUser struct {
+	ID        string `json:"https://slack.com/user_id"`
+	Email     string `json:"email"`
+	Name      string `json:"name"`
+	AvatarURL string `json:"picture"`
+	TeamID    string `json:"https://slack.com/team_id"`
+}
+
+
+// NewSlackOIDCProvider creates a Slack account provider with Sign in with Slack.
 func NewSlackOIDCProvider(ext conf.OAuthProviderConfiguration, scopes string) (OAuthProvider, error) {
 	if err := ext.ValidateOAuth(); err != nil {
 		return nil, err
 	}
 
-	apiPath := chooseHost(ext.URL, defaultSlackApiBase) + "/api"
-	authPath := chooseHost(ext.URL, defaultSlackApiBase) + "/openid"
+	apiPath := chooseHost(ext.URL, defaultSlackOIDCApiBase) + "/api"
+	authPath := chooseHost(ext.URL, defaultSlackOIDCApiBase) + "/openid"
 
 	oauthScopes := []string{
 		"profile",
@@ -26,7 +43,7 @@ func NewSlackOIDCProvider(ext conf.OAuthProviderConfiguration, scopes string) (O
 		oauthScopes = append(oauthScopes, strings.Split(scopes, ",")...)
 	}
 
-	return &slackProvider{
+	return &slackOIDCProvider{
 		Config: &oauth2.Config{
 			ClientID:     ext.ClientID[0],
 			ClientSecret: ext.Secret,
@@ -40,3 +57,40 @@ func NewSlackOIDCProvider(ext conf.OAuthProviderConfiguration, scopes string) (O
 		APIPath: apiPath,
 	}, nil
 }
+
+func (g slackOIDCProvider) GetOAuthToken(code string) (*oauth2.Token, error) {
+	return g.Exchange(context.Background(), code)
+}
+
+func (g slackOIDCProvider) GetUserData(ctx context.Context, tok *oauth2.Token) (*UserProvidedData, error) {
+	var u slackOIDCUser
+	if err := makeRequest(ctx, tok, g.Config, g.APIPath+"/openid.connect.userInfo", &u); err != nil {
+		return nil, err
+	}
+
+	data := &UserProvidedData{}
+	if u.Email != "" {
+		data.Emails = []Email{{
+			Email:    u.Email,
+			Verified: true, // Slack doesn't provide data on if email is verified.
+			Primary:  true,
+		}}
+	}
+
+	data.Metadata = &Claims{
+		Issuer:  g.APIPath,
+		Subject: u.ID,
+		Name:    u.Name,
+		Picture: u.AvatarURL,
+		CustomClaims: map[string]interface{}{
+			"https://slack.com/team_id": u.TeamID,
+		},
+
+		// To be deprecated
+		AvatarURL:  u.AvatarURL,
+		FullName:   u.Name,
+		ProviderId: u.ID,
+	}
+	return data, nil
+}
+
