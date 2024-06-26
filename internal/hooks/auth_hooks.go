9568d1ac86f73e723b86567c131067a6ba339bfd
Kang Ming <kang.ming1996@gmail.com>
2024-06-26 17:24:20 +0800

    fix: update token jsonschema

--- internal/hooks/auth_hooks.go
+++ internal/hooks/auth_hooks.go
@@ -43,7 +43,7 @@ const MinimumViableTokenSchema = `{
   "type": "object",
   "properties": {
     "aud": {
-      "type": "string"
+      "type": "array"
     },
     "exp": {
       "type": "integer"
