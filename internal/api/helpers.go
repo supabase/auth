e8995b8819a84e7b208cdd9c12685a6dbd57be9b
Kang Ming <kang.ming1996@gmail.com>
2024-06-26 17:09:48 +0800

    chore: fix staticcheck

--- internal/api/helpers.go
+++ internal/api/helpers.go
@@ -36,10 +36,12 @@ func (a *API) requestAud(ctx context.Context, r *http.Request) string {
 
 	// Then check the token
 	claims := getClaims(ctx)
-	aud, _ := claims.GetAudience()
 
-	if claims != nil && len(aud) != 0 {
-		return aud[0]
+	if claims != nil {
+		aud, _ := claims.GetAudience()
+		if len(aud) != 0 {
+			return aud[0]
+		}
 	}
 
 	// Finally, return the default if none of the above methods are successful
