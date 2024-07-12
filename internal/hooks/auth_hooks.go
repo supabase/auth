98d83245e40d606438eb0afdbf474276179fd91d
Kang Ming <kang.ming1996@gmail.com>
2024-07-12 09:20:55 -0700

    fix: serialize jwt as string (#1657)
    ## What kind of change does this PR introduce?
    * serializes the jwt as a string rather than a slice of strings
    
    ## What is the current behavior?
    
    Please link any relevant issues here.
    
    ## What is the new behavior?
    
    Feel free to include screenshots if it includes visual changes.
    
    ## Additional context
    
    Add any other context or screenshots.

--- internal/hooks/auth_hooks.go
+++ internal/hooks/auth_hooks.go
@@ -43,7 +43,7 @@ const MinimumViableTokenSchema = `{
   "type": "object",
   "properties": {
     "aud": {
-      "type": "array"
+      "type": ["string", "array"]
     },
     "exp": {
       "type": "integer"
