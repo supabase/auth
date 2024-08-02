27d97fab8868bb390b8cd619848b85396167be17
joel <lee.yi.jie.joel@gmail.com>
2024-08-02 22:12:14 +0200

    fix: clean up migration

--- migrations/20240802193726_add_mfa_factors_column_last_challenged_at.sql
+++ migrations/20240802193726_add_mfa_factors_column_last_challenged_at.sql
@@ -1,14 +1,1 @@
-do $$ begin
-    alter type {{ index .Options "Namespace" }}.factor_type add value 'phone';
-exception
-    when duplicate_object then null;
-end $$;
-
-
-alter table {{ index .Options "Namespace" }}.mfa_factors add column if not exists phone text unique default null;
-alter table {{ index .Options "Namespace" }}.mfa_challenges add column if not exists sent_at timestamptz null;
-alter table {{ index .Options "Namespace" }}.mfa_challenges add column if not exists otp_code text null;
-
-create index if not exists idx_sent_at on {{ index .Options "Namespace" }}.mfa_challenges(sent_at);
-
-create unique index if not exists unique_verified_phone_factor on {{ index .Options "Namespace" }}.mfa_factors (user_id, phone);
+alter table {{ index .Options "Namespace" }}.mfa_factors add column if not exists last_challenged_at timestamptz unique default null;
