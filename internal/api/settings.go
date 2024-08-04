078c3a8adcd51e57b68ab1b582549f5813cccd14
Joel Lee <lee.yi.jie.joel@gmail.com>
2024-08-04 20:02:18 +0200

    fix: drop the MFA_ENABLED config (#1701)
    ## What kind of change does this PR introduce?
    
    The `MFA_ENABLED` config is deprecated and not in active use.

--- internal/api/settings.go
+++ internal/api/settings.go
@@ -36,7 +36,6 @@ type Settings struct {
 	MailerAutoconfirm bool             `json:"mailer_autoconfirm"`
 	PhoneAutoconfirm  bool             `json:"phone_autoconfirm"`
 	SmsProvider       string           `json:"sms_provider"`
-	MFAEnabled        bool             `json:"mfa_enabled"`
 	SAMLEnabled       bool             `json:"saml_enabled"`
 }
 
@@ -75,7 +74,6 @@ func (a *API) Settings(w http.ResponseWriter, r *http.Request) error {
 		MailerAutoconfirm: config.Mailer.Autoconfirm,
 		PhoneAutoconfirm:  config.Sms.Autoconfirm,
 		SmsProvider:       config.Sms.Provider,
-		MFAEnabled:        config.MFA.Enabled,
 		SAMLEnabled:       config.SAML.Enabled,
 	})
 }
