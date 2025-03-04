bf4256ae6af8c8d82090a333cc482242387539b6
Kang Ming <kang.ming1996@gmail.com>
2025-03-04 10:08:43 +0100

    fix: update figma token endpoint

--- internal/api/provider/figma.go
+++ internal/api/provider/figma.go
@@ -51,7 +51,7 @@ func NewFigmaProvider(ext conf.OAuthProviderConfiguration, scopes string) (OAuth
 			ClientSecret: ext.Secret,
 			Endpoint: oauth2.Endpoint{
 				AuthURL:  authHost + "/oauth",
-				TokenURL: authHost + "/api/oauth/token",
+				TokenURL: apiHost + "/v1/oauth/token",
 			},
 			RedirectURL: ext.RedirectURI,
 			Scopes:      oauthScopes,
