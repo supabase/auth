8b233820e41fedd18338eb37345ecbb0beb350ce
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-04-10 17:57:52 +0200

    feat: separate web3 rate limits from other `/token?grant_type=...` (#1985)
    Sign in with Web3 can be very prone to abuse. This is because all it
    takes for the `/token?grant_type=web3` endpoint to be abused is the
    generation of signed messages by a private key.
    
    For this reason a new rate limit config is provided
    `GOTRUE_RATE_LIMIT_WEB3` which limits the number of such calls per IP
    address.
    
    To achieve this, the rate limit enforcement on the `/token` API is moved
    inside in the `Token` handler. This provides a future basis for
    separating out the rate limiters for Sign in with Password and ID token,
    as well as rate limiters for PKCE.

--- internal/api/token_test.go
+++ internal/api/token_test.go
@@ -225,7 +225,7 @@ func (ts *TokenTestSuite) TestSingleSessionPerUserNoTags() {
 
 func (ts *TokenTestSuite) TestRateLimitTokenRefresh() {
 	var buffer bytes.Buffer
-	req := httptest.NewRequest(http.MethodPost, "http://localhost/token", &buffer)
+	req := httptest.NewRequest(http.MethodPost, "http://localhost/token?grant_type=refresh_token", &buffer)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("My-Custom-Header", "1.2.3.4")
 
@@ -246,7 +246,38 @@ func (ts *TokenTestSuite) TestRateLimitTokenRefresh() {
 	assert.Equal(ts.T(), http.StatusTooManyRequests, w.Code)
 
 	// It doesn't rate limit a new value for the limited header
-	req = httptest.NewRequest(http.MethodPost, "http://localhost/token", &buffer)
+	req = httptest.NewRequest(http.MethodPost, "http://localhost/token?grant_type=refresh_token", &buffer)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("My-Custom-Header", "5.6.7.8")
+	w = httptest.NewRecorder()
+	ts.API.handler.ServeHTTP(w, req)
+	assert.Equal(ts.T(), http.StatusBadRequest, w.Code)
+}
+
+func (ts *TokenTestSuite) TestRateLimitWeb3() {
+	var buffer bytes.Buffer
+	req := httptest.NewRequest(http.MethodPost, "http://localhost/token?grant_type=web3", &buffer)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("My-Custom-Header", "1.2.3.4")
+
+	// It rate limits after 30 requests
+	for i := 0; i < 30; i++ {
+		w := httptest.NewRecorder()
+		ts.API.handler.ServeHTTP(w, req)
+		assert.Equal(ts.T(), http.StatusBadRequest, w.Code)
+	}
+	w := httptest.NewRecorder()
+	ts.API.handler.ServeHTTP(w, req)
+	assert.Equal(ts.T(), http.StatusTooManyRequests, w.Code)
+
+	// It ignores X-Forwarded-For by default
+	req.Header.Set("X-Forwarded-For", "1.1.1.1")
+	w = httptest.NewRecorder()
+	ts.API.handler.ServeHTTP(w, req)
+	assert.Equal(ts.T(), http.StatusTooManyRequests, w.Code)
+
+	// It doesn't rate limit a new value for the limited header
+	req = httptest.NewRequest(http.MethodPost, "http://localhost/token?grant_type=web3", &buffer)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("My-Custom-Header", "5.6.7.8")
 	w = httptest.NewRecorder()
