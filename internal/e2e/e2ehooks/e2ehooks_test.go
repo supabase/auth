9b052b9c374de51923c4897bf10507afdf90bb3f
Chris Stockton <chris.stockton@supabase.io>
2025-05-07 09:05:02 -0700

    chore: remove some commented code and v0hooks

--- internal/e2e/e2ehooks/e2ehooks_test.go
+++ internal/e2e/e2ehooks/e2ehooks_test.go
@@ -113,6 +113,26 @@ func TestHookRecorder(t *testing.T) {
 			name: v1hooks.AfterUserCreated,
 			hook: hookRec.AfterUserCreated,
 		},
+		{
+			name: v0hooks.CustomizeAccessToken,
+			hook: hookRec.CustomizeAccessToken,
+		},
+		{
+			name: v0hooks.MFAVerification,
+			hook: hookRec.MFAVerification,
+		},
+		{
+			name: v0hooks.PasswordVerification,
+			hook: hookRec.PasswordVerification,
+		},
+		{
+			name: v0hooks.SendEmail,
+			hook: hookRec.SendEmail,
+		},
+		{
+			name: v0hooks.SendSMS,
+			hook: hookRec.SendSMS,
+		},
 	}
 
 	for _, test := range tests {
@@ -160,97 +180,3 @@ func TestHookRecorder(t *testing.T) {
 		}
 	}
 }
-
-/*
-
-func TestUserHooks(t *testing.T) {
-	t.SkipNow()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	globalCfg, err := conf.LoadGlobal("../../../hack/test.env")
-	if err != nil {
-		t.Fatalf("exp nil err; got %v", err)
-	}
-
-	inst, err := New(globalCfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer inst.Close()
-
-	apiCli := inst.APIClient
-	apiSrv := inst.APIServer
-	hookRec := inst.HookRecorder
-
-	// Basic tests for Before/After User Created hooks
-	{
-
-		// Signup a user
-		var signupUser *models.User
-		email := "e2etesthooks_" + uuid.Must(uuid.NewV4()).String() + "@localhost"
-		{
-			req := &api.SignupParams{
-				Email:    email,
-				Password: "password",
-			}
-			res := new(models.User)
-			err := apiCli.Do(ctx, http.MethodPost, apiSrv.URL+"/signup", req, res)
-			if err != nil {
-				t.Fatalf("exp nil err; got %v", err)
-			}
-			signupUser = res
-
-			require.Equal(t, email, signupUser.Email.String())
-		}
-
-		{
-			calls := hookRec.BeforeUserCreated.GetCalls()
-			if exp, got := 1, len(calls); exp != got {
-				t.Fatalf("exp %v; got %v", exp, got)
-			}
-			call := calls[0]
-
-			hookReq := &v1hooks.BeforeUserCreatedRequest{}
-			if err := call.Unmarshal(hookReq); err != nil {
-				t.Fatalf("exp nil err; got %v", err)
-			}
-
-			u := hookReq.User
-			require.Equal(t, signupUser.ID, u.ID)
-			require.Equal(t, signupUser.Aud, u.Aud)
-			require.Equal(t, signupUser.Email, u.Email)
-			require.Equal(t, signupUser.AppMetaData, u.AppMetaData)
-
-			require.True(t, u.CreatedAt.IsZero())
-			require.True(t, u.UpdatedAt.IsZero())
-		}
-
-		{
-			calls := hookRec.AfterUserCreated.GetCalls()
-			if exp, got := 1, len(calls); exp != got {
-				t.Fatalf("exp %v; got %v", exp, got)
-			}
-			call := calls[0]
-
-			hookReq := &v1hooks.AfterUserCreatedRequest{}
-			if err := call.Unmarshal(hookReq); err != nil {
-				t.Fatalf("exp nil err; got %v", err)
-			}
-
-			u := hookReq.User
-			require.Equal(t, signupUser.ID, u.ID)
-			require.Equal(t, signupUser.Aud, u.Aud)
-			require.Equal(t, signupUser.Role, u.Role)
-			require.Equal(t, signupUser.Email, u.Email)
-			require.Equal(t, signupUser.AppMetaData, u.AppMetaData)
-
-			require.Equal(t, signupUser.CreatedAt, u.CreatedAt)
-			require.True(t, signupUser.CreatedAt.Before(u.UpdatedAt))
-			require.True(t, signupUser.UpdatedAt.After(u.UpdatedAt))
-		}
-	}
-}
-
-*/
