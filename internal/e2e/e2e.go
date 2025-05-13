36d8de3dc970efdf340076b2cb6a40ea38c38f6d
Chris Stockton <chris.stockton@supabase.io>
2025-05-13 11:06:53 -0700

    feat: add support packages for end-to-end testing
    Added 3 new packages in the "e2e" directory to make
    writing tests that needed an API server, hook server or
    both easier. Also added some simple helpers for config
    and connection loading from within unit tests.
    
    The packages:
    - Package e2e provides a few utilities for use in unit tests.
    - Package e2eapi provides utilities for end-to-end testing the api.
    - Package e2ehooks provides utilities for end-to-end testing of hooks.

---
+++ internal/e2e/e2e.go
@@ -0,0 +1,62 @@
+// Package e2e provides a few utilities for use in unit tests.
+package e2e
+
+import (
+	"path/filepath"
+	"runtime"
+
+	"github.com/supabase/auth/internal/conf"
+	"github.com/supabase/auth/internal/storage"
+	"github.com/supabase/auth/internal/storage/test"
+)
+
+var (
+	projectRoot string
+	configPath  string
+)
+
+func init() {
+	_, thisFile, _, _ := runtime.Caller(0)
+	projectRoot = filepath.Join(filepath.Dir(thisFile), "../..")
+	configPath = filepath.Join(GetProjectRoot(), "hack", "test.env")
+}
+
+// GetProjectRoot returns the path to the root of the project. This may be used
+// to locate files without needing the relative path from a given test.
+func GetProjectRoot() string {
+	return projectRoot
+}
+
+// GetConfigPath returns the path for the "/hack/test.env" config file.
+func GetConfigPath() string {
+	return configPath
+}
+
+// Config calls conf.LoadGlobal using GetConfigPath().
+func Config() (*conf.GlobalConfiguration, error) {
+	globalCfg, err := conf.LoadGlobal(GetConfigPath())
+	if err != nil {
+		return nil, err
+	}
+	return globalCfg, nil
+}
+
+// Conn returns a connection for the given config.
+func Conn(globalCfg *conf.GlobalConfiguration) (*storage.Connection, error) {
+	conn, err := test.SetupDBConnection(globalCfg)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+// Must may be used by Config and Conn, i.e.:
+//
+//	cfg := e2e.Must(e2e.Config())
+//	conn := e2e.Must(e2e.Conn(cfg))
+func Must[T any](res T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
