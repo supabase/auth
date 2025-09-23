fe0fd04c9f5558d0165a94c7c080fb15c036d08f
issuedat <165281975+issuedat@users.noreply.github.com>
2025-09-23 14:44:50 +0200

    feat: password changed email notification (#2176)
    This PR adds support for sending the user an email notification when
    their password has been changed.
    
    3 new environment variables are introduced:
    
    - `GOTRUE_MAILER_SUBJECTS_PASSWORD_CHANGED_NOTIFICATION`: Email subject
    to use for password changed notification.
    - `GOTRUE_MAILER_TEMPLATES_PASSWORD_CHANGED_NOTIFICATION`: The URL to
    specify a custom template.
    -
    `GOTRUE_MAILER_NOTIFICATION_CONFIGURATIONS_PASSWORD_CHANGED_NOTIFICATION_ENABLED`:
    whether the notification is enabled or not.
    
    The feature is disabled by default. To enable it, the
    `GOTRUE_MAILER_NOTIFICATION_CONFIGURATIONS_PASSWORD_CHANGED_NOTIFICATION_ENABLED`
    environment variable must be set to `true`.
    
    The default email will look as follows:
    
    <img width="828" height="472" alt="default"
    src="https://github.com/user-attachments/assets/70588e93-b8aa-4bb9-82d9-b9e898aa7035"
    />

---
+++ internal/mailer/mockclient/mockclient.go
@@ -0,0 +1,164 @@
+package mockclient
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/supabase/auth/internal/models"
+)
+
+// MockMailer implements the mailer.Mailer interface for testing
+type MockMailer struct {
+	InviteMailCalls          []InviteMailCall
+	ConfirmationMailCalls    []ConfirmationMailCall
+	RecoveryMailCalls        []RecoveryMailCall
+	MagicLinkMailCalls       []MagicLinkMailCall
+	EmailChangeMailCalls     []EmailChangeMailCall
+	ReauthenticateMailCalls  []ReauthenticateMailCall
+	GetEmailActionLinkCalls  []GetEmailActionLinkCall
+	PasswordChangedMailCalls []PasswordChangedMailCall
+}
+
+type InviteMailCall struct {
+	User        *models.User
+	OTP         string
+	ReferrerURL string
+	ExternalURL *url.URL
+}
+
+type ConfirmationMailCall struct {
+	User        *models.User
+	OTP         string
+	ReferrerURL string
+	ExternalURL *url.URL
+}
+
+type RecoveryMailCall struct {
+	User        *models.User
+	OTP         string
+	ReferrerURL string
+	ExternalURL *url.URL
+}
+
+type MagicLinkMailCall struct {
+	User        *models.User
+	OTP         string
+	ReferrerURL string
+	ExternalURL *url.URL
+}
+
+type EmailChangeMailCall struct {
+	User        *models.User
+	OTPNew      string
+	OTPCurrent  string
+	ReferrerURL string
+	ExternalURL *url.URL
+}
+
+type ReauthenticateMailCall struct {
+	User *models.User
+	OTP  string
+}
+
+type GetEmailActionLinkCall struct {
+	User        *models.User
+	ActionType  string
+	ReferrerURL string
+	ExternalURL *url.URL
+	Result      string
+	Error       error
+}
+
+type PasswordChangedMailCall struct {
+	User *models.User
+}
+
+func (m *MockMailer) InviteMail(r *http.Request, user *models.User, otp, referrerURL string, externalURL *url.URL) error {
+	m.InviteMailCalls = append(m.InviteMailCalls, InviteMailCall{
+		User:        user,
+		OTP:         otp,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+	})
+	return nil
+}
+
+func (m *MockMailer) ConfirmationMail(r *http.Request, user *models.User, otp, referrerURL string, externalURL *url.URL) error {
+	m.ConfirmationMailCalls = append(m.ConfirmationMailCalls, ConfirmationMailCall{
+		User:        user,
+		OTP:         otp,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+	})
+	return nil
+}
+
+func (m *MockMailer) RecoveryMail(r *http.Request, user *models.User, otp, referrerURL string, externalURL *url.URL) error {
+	m.RecoveryMailCalls = append(m.RecoveryMailCalls, RecoveryMailCall{
+		User:        user,
+		OTP:         otp,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+	})
+	return nil
+}
+
+func (m *MockMailer) MagicLinkMail(r *http.Request, user *models.User, otp, referrerURL string, externalURL *url.URL) error {
+	m.MagicLinkMailCalls = append(m.MagicLinkMailCalls, MagicLinkMailCall{
+		User:        user,
+		OTP:         otp,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+	})
+	return nil
+}
+
+func (m *MockMailer) EmailChangeMail(r *http.Request, user *models.User, otpNew, otpCurrent, referrerURL string, externalURL *url.URL) error {
+	m.EmailChangeMailCalls = append(m.EmailChangeMailCalls, EmailChangeMailCall{
+		User:        user,
+		OTPNew:      otpNew,
+		OTPCurrent:  otpCurrent,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+	})
+	return nil
+}
+
+func (m *MockMailer) ReauthenticateMail(r *http.Request, user *models.User, otp string) error {
+	m.ReauthenticateMailCalls = append(m.ReauthenticateMailCalls, ReauthenticateMailCall{
+		User: user,
+		OTP:  otp,
+	})
+	return nil
+}
+
+func (m *MockMailer) GetEmailActionLink(user *models.User, actionType, referrerURL string, externalURL *url.URL) (string, error) {
+	call := GetEmailActionLinkCall{
+		User:        user,
+		ActionType:  actionType,
+		ReferrerURL: referrerURL,
+		ExternalURL: externalURL,
+		Result:      "http://example.com/action",
+		Error:       nil,
+	}
+	m.GetEmailActionLinkCalls = append(m.GetEmailActionLinkCalls, call)
+	return call.Result, call.Error
+}
+
+func (m *MockMailer) PasswordChangedNotificationMail(r *http.Request, user *models.User) error {
+	m.PasswordChangedMailCalls = append(m.PasswordChangedMailCalls, PasswordChangedMailCall{
+		User: user,
+	})
+	return nil
+}
+
+func (m *MockMailer) Reset() {
+	m.InviteMailCalls = nil
+	m.ConfirmationMailCalls = nil
+	m.RecoveryMailCalls = nil
+	m.MagicLinkMailCalls = nil
+	m.EmailChangeMailCalls = nil
+	m.ReauthenticateMailCalls = nil
+	m.GetEmailActionLinkCalls = nil
+	m.PasswordChangedMailCalls = nil
+}
