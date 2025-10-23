7db081f23ffb09024c8250b990f3296f1290e59a
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-10-23 10:59:02 +0200

    fix TestSafeIntegers

--- internal/crypto/refresh_tokens_test.go
+++ internal/crypto/refresh_tokens_test.go
@@ -17,7 +17,7 @@ func TestSafeIntegers(t *testing.T) {
 	require.Equal(t, int64(math.MaxInt64), safeInt64(math.MaxInt64))
 
 	require.Equal(t, int64(0), safeUint64(-1))
-	require.Equal(t, int64(math.MaxInt64), safeInt64(math.MaxInt64))
+	require.Equal(t, int64(math.MaxInt64), safeUint64(math.MaxInt64))
 }
 
 func TestRefreshTokenParse(t *testing.T) {
