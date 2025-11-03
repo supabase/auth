a28ac60d50d93cffcb2986cf704080cf6a5b25dd
Donny Pratama <pratamatama1024@gmail.com>
2025-11-03 18:21:31 +0700

    feat: test case

--- internal/utilities/siwk/parser_test.go
+++ internal/utilities/siwk/parser_test.go
@@ -25,63 +25,63 @@ func TestParseMessage(t *testing.T) {
 			error:   ErrInvalidHeader,
 		},
 		{
-			example: "******* wants you to sign in with your Ethereum account:\n\n\n\n\n\n",
+			example: "******* wants you to sign in with your Kaspa account:\n\n\n\n\n\n",
 			error:   ErrInvalidDomain,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n***************************************\n\n\n\n\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\n***************************************\n\n\n\n\n",
 			error:   ErrInvalidAddress,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\nURI: https://google.com\n\n\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\nURI: https://google.com\n\n\n",
 			error:   ErrThirdLineNotEmpty,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nNot Parsable\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nNot Parsable\n",
 			error:   errUnparsableLine(5),
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: ***\nIssued At: 2025-01-01T00:00:00Z",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: ***\nIssued At: 2025-01-01T00:00:00Z",
 			error:   ErrInvalidURI,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: not-a-timestamp",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: not-a-timestamp",
 			error:   ErrInvalidIssuedAt,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: 2025-01-01T00:00:00Z\nExpiration Time: not-a-timestamp",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: 2025-01-01T00:00:00Z\nExpiration Time: not-a-timestamp",
 			error:   ErrInvalidExpirationTime,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: 2025-01-01T00:00:00Z\nNot Before: not-a-timestamp",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://google.com\nIssued At: 2025-01-01T00:00:00Z\nNot Before: not-a-timestamp",
 			error:   ErrInvalidNotBefore,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 2\nIssued At: 2025-01-01T00:00:00Z\nURI: https://google.com\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 2\nIssued At: 2025-01-01T00:00:00Z\nURI: https://google.com\n",
 			error:   errUnsupportedVersion("2"),
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nIssued At: 2025-01-01T00:00:00Z\n\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nIssued At: 2025-01-01T00:00:00Z\n\n",
 			error:   ErrMissingURI,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nResources:\n- https://google.com\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nResources:\n- https://google.com\n",
 			error:   ErrMissingIssuedAt,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-02T00:00:00Z\nExpiration Time: 2025-01-01T00:00:00Z\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-02T00:00:00Z\nExpiration Time: 2025-01-01T00:00:00Z\n",
 			error:   ErrIssuedAfterExpiration,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nExpiration Time: 2025-01-02T00:00:00Z\nNot Before: 2025-01-03T00:00:00Z\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nExpiration Time: 2025-01-02T00:00:00Z\nNot Before: 2025-01-03T00:00:00Z\n",
 			error:   ErrNotBeforeAfterExpiration,
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nResources:\n- https://google.com\n- ***\n",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nStatement\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nResources:\n- https://google.com\n- ***\n",
 			error:   errInvalidResource(1),
 		},
 		{
-			example: "domain.com wants you to sign in with your Ethereum account:\n0x742d35Cc6635C0532925a3b8D53d6e8b3f56ddF1\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nChain ID: random:mainnet",
+			example: "domain.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nVersion: 1\nURI: https://domain.com\nIssued At: 2025-01-01T00:00:00Z\nNetwork ID: random:mainnet",
 			error:   ErrInvalidNetworkID,
 		},
 	}
@@ -100,13 +100,12 @@ func TestParseMessage(t *testing.T) {
 		signature string
 	}{
 		{
-			message:   "example.com wants you to sign in with your Ethereum account:\n0x196a28d05bA75C8dC35B0F6e71DD622D1aC82b7E\n\nSign in to Example App\n\nURI: https://example.com\nVersion: 1\nChain ID: 1\nNonce: 12345678\nIssued At: 2025-01-01T00:00:00.000Z",
-			signature: "0xee337880f195524c156b8cc5f425ffcedb9d94638a91fa41ba72e26d93f04c9d1c7bca7020071c34ef7527ed6389ee24b59de79deab4e9e8251e6ca1e195a56a1b",
+			message:   "example.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nSign in to Example App\n\nURI: https://example.com\nVersion: 1\nNetwork ID: kaspa_mainnet\nNonce: 12345678\nIssued At: 2025-11-03T10:55:43.367Z",
+			signature: "a8dd1b0eed80071ed22bed2392aa0a819e5c9fa5f62090aa89ba3a4cde531ec1460648b2cc84a416b0363d851896c9187f8afbccb0a931ec6bc11e9cb8b61543",
 		},
-		// Address: 0x196a28d05bA75C8dC35B0F6e71DD622D1aC82b7E
 		{
-			message:   "example.com wants you to sign in with your Ethereum account:\n0x196a28d05bA75C8dC35B0F6e71DD622D1aC82b7E\n\nURI: https://example.com\nVersion: 1\nChain ID: 1\nNonce: 12345678\nIssued At: 2025-01-01T00:00:00.000Z",
-			signature: "0x0851224c203d08ced345bc99e66ac531eafbbb54eff94f7297b54ec19a0db7e879c4d246d45e0e13c9c2801db1f71f283c373b8c10cc0a91fe6418220a0aa5391b",
+			message:   "example.com wants you to sign in with your Kaspa account:\nkaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn\n\nURI: https://example.com\nVersion: 1\nNetwork ID: kaspa_mainnet\nNonce: 12345678\nIssued At: 2025-11-03T10:55:43.367Z",
+			signature: "598ad18b808c5d625427dd917f1abef10e5c672d958e7e003e21b50df2fc0d3cd20bf99fb2278ab5b5865fa35108e53ab39aa3af0598cd4f2299867f682b85ec",
 		},
 	}
 
@@ -116,7 +115,7 @@ func TestParseMessage(t *testing.T) {
 
 			require.Nil(t, err)
 			require.Equal(t, "example.com", parsed.Domain)
-			require.Equal(t, "0x196a28d05bA75C8dC35B0F6e71DD622D1aC82b7E", parsed.Address)
+			require.Equal(t, "kaspa:qqk948c2dy6cp0vdg7fqx9xttc47q4qdazunhmfv8u24v77uvmxhycc2uj3yn", parsed.Address)
 
 			if i == 0 {
 				require.Equal(t, "Sign in to Example App", *parsed.Statement)
@@ -124,9 +123,10 @@ func TestParseMessage(t *testing.T) {
 				require.Nil(t, parsed.Statement)
 			}
 
-			require.Equal(t, "2025-01-01 00:00:00 +0000 UTC", parsed.IssuedAt.String())
+			println(parsed.IssuedAt.String())
+			require.Equal(t, "2025-11-03 10:55:43.367 +0000 UTC", parsed.IssuedAt.String())
 			require.Equal(t, "https://example.com", parsed.URI.String())
-			require.Equal(t, "1", parsed.ChainID)
+			require.Equal(t, "kaspa_mainnet", parsed.NetworkID)
 			require.Equal(t, "12345678", parsed.Nonce)
 			// require.Equal(t, "abcdef", *parsed.RequestID)
 
