fa971754903efe6b03ab62c804fbba6a10c97a7f
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-11-05 17:00:39 +0100

    fix: hostname can be empty with redirect urls

--- internal/utilities/request.go
+++ internal/utilities/request.go
@@ -106,7 +106,7 @@ func IsRedirectURLValid(config *conf.GlobalConfiguration, redirectURL string) bo
 		return false
 	} else if ip := net.ParseIP(refurl.Hostname()); ip != nil {
 		return ip.IsLoopback()
-	} else if !regularHostname.MatchString(refurl.Hostname()) {
+	} else if regularHostname != "" && !regularHostname.MatchString(refurl.Hostname()) {
 		// hostname uses characters that are not typically used
 		return false
 	}
