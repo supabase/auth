f5a4cbac73de28cc4b04c5c9725b70517cb131d3
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-11-05 17:59:22 +0100

    fix: hostname can be empty with redirect urls (#2241)
    Oops. In in-app URLs like `com.app://` hostname is empty, but the regex
    expects at least one char.

--- internal/utilities/request.go
+++ internal/utilities/request.go
@@ -101,12 +101,15 @@ func IsRedirectURLValid(config *conf.GlobalConfiguration, redirectURL string) bo
 		return false
 	}
 
+	scheme := strings.ToLower(refurl.Scheme)
+	isHTTP := scheme == "http:" || scheme == "https:"
+
 	if decimalIPAddressPattern.MatchString(refurl.Hostname()) {
 		// IP address in decimal form also not allowed in redirects!
 		return false
 	} else if ip := net.ParseIP(refurl.Hostname()); ip != nil {
 		return ip.IsLoopback()
-	} else if !regularHostname.MatchString(refurl.Hostname()) {
+	} else if isHTTP && !regularHostname.MatchString(refurl.Hostname()) {
 		// hostname uses characters that are not typically used
 		return false
 	}
