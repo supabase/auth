cad340ada0f9b7a9580b366e4214b81a94bcc50e
mo khan <mo.khan@supabase.io>
2026-08-07 11:25:53 -0600

    refactor: delegate to srv.NotFound()

--- internal/api/scim/users.go
+++ internal/api/scim/users.go
@@ -15,21 +15,17 @@ func (srv *Server) UserByID(w http.ResponseWriter, r *http.Request) error {
 
 	id, err := uuid.FromString(chi.URLParam(r, "id"))
 	if err != nil {
-		return userNotFound(w)
+		return srv.NotFound(w, r)
 	}
 
 	provider := shared.GetSSOProvider(ctx)
 	user, err := provider.FindProvisionedUserByID(srv.db.WithContext(ctx), id)
 	if err != nil {
 		if models.IsNotFoundError(err) {
-			return userNotFound(w)
+			return srv.NotFound(w, r)
 		}
 		return srv.internalError(w, r, err)
 	}
 
 	return protocol.Send(w, http.StatusOK, srv.users.MapFrom(user))
-}
-
-func userNotFound(w http.ResponseWriter) error {
-	return protocol.SendError(w, http.StatusNotFound, "", "Resource not found")
 }
