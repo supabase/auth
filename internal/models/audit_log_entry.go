2d4680ef054bedc898da1dbac1b6a7cb9cf5b5af
fadymak <dev@fadymak.com>
2026-08-12 13:47:30 +0200

    feat(auditlogs): introduce identity linked action

--- internal/models/audit_log_entry.go
+++ internal/models/audit_log_entry.go
@@ -41,6 +41,7 @@ const (
 	VerifyFactorAction              AuditAction = "verification_attempted"
 	DeleteFactorAction              AuditAction = "factor_deleted"
 	UpdateFactorAction              AuditAction = "factor_updated"
+	IdentityLinkAction              AuditAction = "identity_linked"
 	IdentityUnlinkAction            AuditAction = "identity_unlinked"
 	PasskeyCreatedAction            AuditAction = "passkey_created"
 	PasskeyUpdatedAction            AuditAction = "passkey_updated"
@@ -68,6 +69,7 @@ var ActionLogTypeMap = map[AuditAction]auditLogType{
 	UserConfirmationRequestedAction: user,
 	UserRepeatedSignUpAction:        user,
 	UserUpdatePasswordAction:        user,
+	IdentityLinkAction:              user,
 	IdentityUnlinkAction:            user,
 	EnrollFactorAction:              factor,
 	UnenrollFactorAction:            factor,
