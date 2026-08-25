ecce30ba7cb5dd2e62cb961e3a1d54b97978b4c3
fadymak <dev@fadymak.com>
2026-08-25 09:24:56 +0200

    chore: rename schema fields for consistency
    - Adds unique user_id to mfa_recovery_code_sets as a workaround to avoid creating a unique index on mfa_factors (user_id) which would impact large tables.
    - Renames fields for consistency
    - Adds unique index on mfa_recovery_codes (mfa_recovery_code_set_id, code_hash) to prevent duplicate codes per set.

--- migrations/20260824000000_add_recovery_codes_factor_type.up.sql
+++ migrations/20260824000000_add_recovery_codes_factor_type.up.sql
@@ -1,6 +1,6 @@
 /* auth_migration: 20260824000000 */
 do $$ begin
-    alter type {{ index .Options "Namespace" }}.factor_type add value 'recovery_codes';
+    alter type {{ index .Options "Namespace" }}.factor_type add value 'recovery_code';
 exception
     when duplicate_object then null;
 end $$;
