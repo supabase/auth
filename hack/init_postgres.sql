a07a9b8e74ff65532ea3e9ef28dd9c1cd6779770
mo khan <mo.khan@supabase.io>
2026-09-01 12:56:55 -0600

    chore: make init_postgres.sql idempotent and per-database

--- hack/init_postgres.sql
+++ hack/init_postgres.sql
@@ -1,7 +1,15 @@
-CREATE USER supabase_admin LOGIN CREATEROLE CREATEDB REPLICATION BYPASSRLS;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'supabase_admin') THEN
+		CREATE USER supabase_admin LOGIN CREATEROLE CREATEDB REPLICATION BYPASSRLS;
+	END IF;
 
--- Supabase super admin
-CREATE USER supabase_auth_admin NOINHERIT CREATEROLE LOGIN NOREPLICATION PASSWORD 'root';
+	IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'supabase_auth_admin') THEN
+		CREATE USER supabase_auth_admin NOINHERIT CREATEROLE LOGIN NOREPLICATION PASSWORD 'root';
+	END IF;
+END
+$$;
+
 CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION supabase_auth_admin;
-GRANT CREATE ON DATABASE postgres TO supabase_auth_admin;
+GRANT CREATE ON DATABASE :"dbname" TO supabase_auth_admin;
 ALTER USER supabase_auth_admin SET search_path = 'auth';
